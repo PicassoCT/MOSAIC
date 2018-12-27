da18de3bb99732be4472b6dc797b58cfea1b2b63
PicassoCT <PicassoCT@gmx.de>
2018-12-27 10:37:22 +0100

    Communication speeds up searching and units

---
+++ units/Neutral/House.lua
@@ -0,0 +1,28 @@
+local House = Building:New{
+	corpse				= "",
+	maxDamage           = 500,
+	mass                = 500,
+	buildCostEnergy     = 5,
+	buildCostMetal      = 5,
+	explodeAs			= "none",
+
+
+	footprintX = 4,
+	footprintZ = 4,
+	script 			= "House.lua",
+	objectName        	= "house.s3o",
+
+
+	
+	customparams = {
+		helptext		= "Civilian Building",
+		baseclass		= "Building", -- TODO: hacks
+    },
+}
+
+
+return lowerkeys({
+	--Temp
+	["house"] = House:New(),
+	
+})
