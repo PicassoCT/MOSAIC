90ec21e0a91a6db3fad12b79ee21616ffac721b9
Florian Seidl -Schulz <PicassoCT@gmx.de>
2019-11-25 00:33:04 +0100

    added unfolding animation

--- scripts/turretscript.lua
+++ scripts/turretscript.lua
@@ -18,23 +18,45 @@ function script.Create()
 	resetAll(unitID)
 	Hide(aimpiece)
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
+	hideT(TablesOfPiecesGroups["TBase"])
 	StartThread(unfold)
+
 end
 
 function unfold()
+	Sleep(10)
 	if isTransported(unitID)== false then
 		Turn(Turret,x_axis,math.rad(90),0)
+		Turn(TablesOfPiecesGroups["TBase"][1],2,math.rad(-30),0)
+		Turn(TablesOfPiecesGroups["TBase"][2],2,math.rad(30),0)
+		Turn(TablesOfPiecesGroups["TBase"][3],2,math.rad(-30),0)
+		Turn(TablesOfPiecesGroups["TBase"][4],2,math.rad(30),0)
 		Sleep(1000)
 		
-		Turn(TablesOfPiecesGroups["UpLeg"][1],y_axis,math.rad(-30),0)
-		Turn(TablesOfPiecesGroups["UpLeg"][2],y_axis,math.rad(30),0)
-		Turn(TablesOfPiecesGroups["UpLeg"][3],y_axis,math.rad(30),0)
-		Turn(TablesOfPiecesGroups["UpLeg"][4],y_axis,math.rad(-30),0)
+
+
+		WaitForTurns(TablesOfPiecesGroups["UpLeg"])
+		
+	
+		for i=1,2 do
+			Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(50),math.pi)
+			Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(-90),math.pi)	
+		end	
+		for i=3,4 do
+			Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(-50),math.pi)
+			Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(90),math.pi)	
+		end
+		WaitForTurns(TablesOfPiecesGroups["LowLeg"])
+		WaitForTurns(TablesOfPiecesGroups["UpLeg"])
+		
+
 
 
 		WTurn(Turret,x_axis,math.rad(0),math.pi)
 	end
+
 end
+
 
 function script.Killed(recentDamage, _)
 		
@@ -55,13 +77,16 @@ function script.QueryWeapon1()
 end
 
 function script.AimWeapon1(Heading, pitch)
+
     --aiming animation: instantly turn the gun towards the enemy
 
-	WTurn(Turret,y_axis, Heading, math.pi)
+	Turn(center,y_axis, Heading, math.pi)
+	WTurn(Turret,x_axis, -pitch, math.pi)
 
 	
     return true
 end
+
 
 
 function script.FireWeapon1()
