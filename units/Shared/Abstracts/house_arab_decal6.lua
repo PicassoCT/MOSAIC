e5261e19e2d463f224a871eb5c409ea81b372953
Florian Seidl -Schulz <PicassoCT@gmx.de>
2019-11-27 00:05:30 +0100

    added a decal with a pool

---
+++ units/Shared/Abstracts/house_arab_decal6.lua
@@ -0,0 +1,80 @@
+local unitName = "house_arab_decal6"
+
+local unitDef = {
+name = "house_arab_decal",
+Description = "ThereForTheDecal",
+objectName = "emptyObjectIsEmpty.s3o",
+script = "decalScript.lua",
+buildPic = "placeholder.png",
+--cost
+buildCostMetal = 0,
+buildCostEnergy = 0,
+buildTime =1,
+--Health
+maxDamage = 1200,
+idleAutoHeal = 0,
+--Movement
+
+FootprintX = 1,
+FootprintZ = 1,
+MaxSlope = 5,
+--MaxVelocity = 0.5,
+MaxWaterDepth =0,
+--MovementClass = "Default2x2",--
+
+
+sightDistance = 300,
+
+reclaimable=true,
+Builder = true,
+CanAttack = false,
+CanGuard = false,
+CanMove = false,
+CanPatrol = false,
+CanStop = false,
+LeaveTracks = false,
+
+	YardMap ="y",
+
+-- Building	
+	  levelGround            = false,	
+	workerTime = 1,
+
+
+
+
+		usebuildinggrounddecal = true,
+
+		buildinggrounddecaldecayspeed = 0.00000002,
+		buildinggrounddecalsizex = 20,
+		buildinggrounddecalsizey = 20,
+		buildinggrounddecaltype = "HouseDecal6.png",
+
+
+
+
+
+ 
+ 
+ 
+
+   
+
+Category = [[LAND]],
+
+
+
+  
+   customParams = {},
+ sfxtypes = {
+				explosiongenerators = {
+			
+				   --
+				    --Bulletof The Cannon
+				},
+
+			},
+
+}
+
+return lowerkeys({ [unitName] = unitDef })
