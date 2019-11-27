97bb1ba3ce9c8fb40e7415138101277846e84c33
Florian Seidl -Schulz <PicassoCT@gmx.de>
2019-11-27 23:27:57 +0100

    adding transport detection to the turret

--- scripts/turretscript.lua
+++ scripts/turretscript.lua
@@ -25,35 +25,45 @@ end
 
 function unfold()
 	Sleep(10)
-	if isTransported(unitID)== false then
-		Turn(Turret,x_axis,math.rad(90),0)
-		Turn(TablesOfPiecesGroups["TBase"][1],2,math.rad(-30),0)
-		Turn(TablesOfPiecesGroups["TBase"][2],2,math.rad(30),0)
-		Turn(TablesOfPiecesGroups["TBase"][3],2,math.rad(-30),0)
-		Turn(TablesOfPiecesGroups["TBase"][4],2,math.rad(30),0)
-		Sleep(1000)
-		
-
-
-		WaitForTurns(TablesOfPiecesGroups["UpLeg"])
-		
+	Turn(Turret,x_axis,math.rad(90),0)
+	Turn(TablesOfPiecesGroups["TBase"][1],2,math.rad(-30),0)
+	Turn(TablesOfPiecesGroups["TBase"][2],2,math.rad(30),0)
+	Turn(TablesOfPiecesGroups["TBase"][3],2,math.rad(-30),0)
+	Turn(TablesOfPiecesGroups["TBase"][4],2,math.rad(30),0)
+	WTurn(Turret,x_axis,math.rad(0),math.pi)
 	
-		for i=1,2 do
-			Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(50),math.pi)
-			Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(-90),math.pi)	
-		end	
-		for i=3,4 do
-			Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(-50),math.pi)
-			Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(90),math.pi)	
-		end
-		WaitForTurns(TablesOfPiecesGroups["LowLeg"])
-		WaitForTurns(TablesOfPiecesGroups["UpLeg"])
+	while true do
+		if isTransported(unitID)== false then
+			Sleep(100)
+			WaitForTurns(TablesOfPiecesGroups["UpLeg"])	
+			for i=1,2 do
+				Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(50),math.pi)
+				Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(-90),math.pi)	
+			end	
+			for i=3,4 do
+				Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(-50),math.pi)
+				Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(90),math.pi)	
+			end
+			WaitForTurns(TablesOfPiecesGroups["LowLeg"])
+			WaitForTurns(TablesOfPiecesGroups["UpLeg"])
+		else
+			WaitForTurns(TablesOfPiecesGroups["UpLeg"])	
+				for i=1,2 do
+					Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(0),math.pi)
+					Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(0),math.pi)	
+				end	
+				for i=3,4 do
+					Turn(TablesOfPiecesGroups["UpLeg"][i],x_axis,math.rad(0),math.pi)
+					Turn(TablesOfPiecesGroups["LowLeg"][i],x_axis,math.rad(0),math.pi)	
+				end
+			WaitForTurns(TablesOfPiecesGroups["LowLeg"])
+			WaitForTurns(TablesOfPiecesGroups["UpLeg"])
 		
-
-
-
-		WTurn(Turret,x_axis,math.rad(0),math.pi)
+		
+		end
+	Sleep(1000)
 	end
+
 
 end
 
