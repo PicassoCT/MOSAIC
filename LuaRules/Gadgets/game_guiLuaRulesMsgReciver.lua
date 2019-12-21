03beeb534547e347a29ef8ec54b2b7f7ffa806d9
Florian Seidl -Schulz <PicassoCT@gmx.de>
2019-12-21 10:46:45 +0100

    added debug gui

---
+++ LuaRules/Gadgets/game_guiLuaRulesMsgReciver.lua
@@ -0,0 +1,42 @@
+function gadget:GetInfo()
+    return {
+        name = "mosaic_guiReciver",
+        desc = "Recives messages from the gui thread",
+        author = "zwzsg",
+        date = "3rd of May 2010",
+        license = "Free",
+        layer = 0,
+        version = 1,
+        enabled = true
+    }
+end
+
+	VFS.Include("scripts/lib_OS.lua")
+	VFS.Include("scripts/lib_UnitScript.lua")
+	VFS.Include("scripts/lib_Animation.lua")
+	VFS.Include("scripts/lib_Build.lua")
+	VFS.Include("scripts/lib_mosaic.lua")
+	
+	
+-- modified the script: only corpses with the customParam "featuredecaytime" will disappear
+
+if (gadgetHandler:IsSyncedCode()) then
+local GameConfig = getGameConfig()
+
+
+    function gadget:RecvLuaMsg(msg, playerID)
+        if msg then
+            Spring.Echo("RecvLuaMsg"..msg)
+			
+			if string.find(msg,"SetGameState:") then
+			msg = msg:gsub("SetGameState:","")
+				if msg and GameConfig.GameState[msg] then
+					GG.GlobalGameState= msg
+					Spring.SetGameRulesParam("GlobalGameState", msg)
+				end
+			end
+	
+	
+        end
+    end
+end
