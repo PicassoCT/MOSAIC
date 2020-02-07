045697691e99ea5b4ce1268a2cb68a5fbf63d41f
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-02-07 13:45:51 +0100

    removed dead code

--- LuaRules/Gadgets/game_buildingPlacementLimitor.lua
+++ LuaRules/Gadgets/game_buildingPlacementLimitor.lua
@@ -11,8 +11,6 @@ function gadget:GetInfo()
     }
 end
 
---GG.UnitsToSpawn:PushCreateUnit(name,x,y,z,dir,teamID)
-
 if (not gadgetHandler:IsSyncedCode()) then
     return
 end
@@ -45,7 +43,7 @@ end
 function gadget:Initialize()
 	mapSizeX, mapSizeZ	=Game.mapSizeX/areaDenyMapResolution, Game.mapSizeZ/ areaDenyMapResolution
 	areaDenyMap = makeTable(0,mapSizeX,mapSizeZ)
-	Spring.Echo(mapSizeX,mapSizeZ)
+	Spring.Echo("Mapsize:",mapSizeX,mapSizeZ)
 	for x=1, mapSizeX do
 		for z=1, mapSizeZ do
 			if x > 0 and z > 0 and x < Game.mapSizeX/areaDenyMapResolution and z < Game.mapSizeZ/areaDenyMapResolution then
@@ -54,9 +52,6 @@ function gadget:Initialize()
 		end
 	end
 end
---https://github.com/ZeroK-RTS/Zero-K/blob/ab984528d14777c188a6161a68d6e1aa201e9a8e/LuaRules/Gadgets/mex_placement.lua#L125
---https://github.com/ZeroK-RTS/Zero-K/blob/f373e6a3c78e7819709aa7a1cd1d4c2500262663/units/staticmex.lua#L10
---https://springrts.com/phpbb/viewtopic.php?f=23&t=35994&p=581356&hilit=SetSquareBuildingMask#p581356
 
 function gadget:UnitDestroyed(unitID, unitDefID, teamID, attackerID)
 	if houseTypeTable[unitDefID] then
