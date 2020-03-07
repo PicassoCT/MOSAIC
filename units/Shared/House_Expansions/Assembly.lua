a661cb93473b728daaa8b5c18ec8990f90fb8348
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-03-07 15:08:53 +0100

    added cruise missile to

--- units/Shared/House_Expansions/Assembly.lua
+++ units/Shared/House_Expansions/Assembly.lua
@@ -52,6 +52,7 @@ local Assembly = Building:New{
 	--ground
 		--turret --snake --walker(roach) --truck
 			"ground_turret_ssied",	"ground_turret_mg",	"ground_turret_antiarmor",
+			"ground_turret_cm_walkerdrop",
 			"ground_truck_assembly", 			"ground_walker_mg",
 			"truck_arab0", "ground_truck_mg", "ground_truck_ssied", "ground_truck_antiarmor",
 			--
