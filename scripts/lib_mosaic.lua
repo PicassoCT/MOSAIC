e4b559959ab3d993c325230bffdea99ff1331d05
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-04-18 23:15:26 +0200

    VERSION{0.662}

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -8,7 +8,7 @@ function getGameConfig()
 	return {
 	instance = {
 	culture = "arabic", -- "international", "european", "chinese", "russia", "northamerica", "southamerica"
-	Version = "Alpha: 0.660",
+	Version = "Alpha: 0.662",
 	},
 		
 	numberOfBuildings 	= 75 *unitFactor,
