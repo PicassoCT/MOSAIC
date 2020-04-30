7cbfb776095e3efe14e4cac34ec558f4bdb78131
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-04-30 21:58:16 +0200

    VERSION{0.669}

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -8,7 +8,7 @@ function getGameConfig()
 	return {
 	instance = {
 	culture = "arabic", -- "international", "european", "chinese", "russia", "northamerica", "southamerica"
-	Version = "Alpha: 0.663",
+	Version = "Alpha: 0.669",
 	},
 		
 	numberOfBuildings 	= 75 *unitFactor,  --not related to the hangdetector bug
