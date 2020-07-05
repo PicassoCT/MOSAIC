ec5112fcc4a8630165bce40e37fd5c70fab06331
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-07-05 23:41:08 +0200

    added missing files

---
+++ Documents/sexpositions/copulation_reference.lua
@@ -0,0 +1,46 @@
+return {
+	{
+		['time'] = 1,
+		['commands'] = {
+			{['c']='turn',['p']=p1_left_u_arm, ['a']=y_axis, ['t']=0.000000, ['s']=1.624962},
+			{['c']='turn',['p']=p1_right_arm, ['a']=x_axis, ['t']=0.000000, ['s']=0.000000},
+			{['c']='turn',['p']=p1_right_arm, ['a']=z_axis, ['t']=1.570796, ['s']=3.255830},
+			{['c']='turn',['p']=p2_right_u_arm, ['a']=x_axis, ['t']=-4.425005, ['s']=4.741077},
+		}
+	},
+	{
+		['time'] = 2,
+		['commands'] = {
+			{['c']='turn',['p']=p2_right_l_arm, ['a']=x_axis, ['t']=0.048183, ['s']=0.051625},
+			{['c']='turn',['p']=p2_right_l_arm, ['a']=y_axis, ['t']=0.029537, ['s']=0.031646},
+			{['c']='turn',['p']=p2_right_l_arm, ['a']=z_axis, ['t']=1.501451, ['s']=1.608698},
+		}
+	},
+	{
+		['time'] = 29,
+		['commands'] = {
+			{['c']='turn',['p']=p2_right_u_arm, ['a']=x_axis, ['t']=0.000000, ['s']=4.425005},
+		}
+	},
+	{
+		['time'] = 30,
+		['commands'] = {
+			{['c']='turn',['p']=p1_left_u_arm, ['a']=y_axis, ['t']=-1.570796, ['s']=1.570796},
+			{['c']='turn',['p']=p1_right_arm, ['a']=x_axis, ['t']=0.000000, ['s']=0.000000},
+			{['c']='turn',['p']=p1_right_arm, ['a']=z_axis, ['t']=-1.576506, ['s']=3.147302},
+			{['c']='turn',['p']=p2_right_l_arm, ['a']=x_axis, ['t']=0.000000, ['s']=0.049845},
+			{['c']='turn',['p']=p2_right_l_arm, ['a']=y_axis, ['t']=0.000000, ['s']=0.030555},
+			{['c']='turn',['p']=p2_right_l_arm, ['a']=z_axis, ['t']=0.000000, ['s']=1.553226},
+		}
+	},
+	{
+		['time'] = 59,
+		['commands'] = {
+		}
+	},
+	{
+		['time'] = 60,
+		['commands'] = {
+		}
+	},
+}
