3edb507f43678820b0e5ad856b551de05d461fd1
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-07-11 09:52:39 +0200

    yay got it workin

--- scripts/civilianOrgyPairScript.lua
+++ scripts/civilianOrgyPairScript.lua
@@ -4,36 +4,77 @@ include "lib_UnitScript.lua"
 include "lib_Animation.lua"
 include "lib_Build.lua"
 
+include "lib_mosaic.lua"
 TablesOfPiecesGroups = {}
-local Animations = include('orgy_couple_ref_movement.lua')
 
- --local orgy_pair = piece("orgy_pair")
- local center = piece("center")
- local p1_l_body = piece("p1_l_body")
- local p1_right_u_leg = piece("p1_right_u_leg")
- local p1_right_l_leg = piece("p1_right_l_leg")
- local p1_left_u_leg = piece("p1_left_u_leg")
- local p1_left_l_leg = piece("p1_left_l_leg")
- local p1_penis = piece("p1_penis")
- local p1_u_body = piece("p1_u_body")
- local p1_right_arm = piece("p1_right_arm")
- local p1_right_l_arm = piece("p1_right_l_arm")
- local p1_left_u_arm = piece("p1_left_u_arm")
- local p1_left_l_arm = piece("p1_left_l_arm")
- local p1_head = piece("p1_head")
- local p2_l_body = piece("p2_l_body")
- local p2_u_body001 = piece("p2_u_body001")
- local p2_right_u_arm = piece("p2_right_u_arm")
- local p2_right_l_arm = piece("p2_right_l_arm")
- local p2_head = piece("p2_head")
- local eye_L001 = piece("eye_L001")
- local eye_L = piece("eye_L")
- local p2_left_u_arm = piece("p2_left_u_arm")
- local p2_left_l_arm = piece("p2_left_l_arm")
- local p2_right_u_leg001 = piece("p2_right_u_leg001")
- local p2_right_l_leg001 = piece("p2_right_l_leg001")
- local p2_right_u_leg = piece("p2_right_u_leg")
- local p2_right_l_leg = piece("p2_right_l_leg")
+local Scene = piece('Scene');
+local center = piece('center');
+local p1_head = piece('p1_head');
+local p1_l_body = piece('p1_l_body');
+local p1_left_l_arm = piece('p1_left_l_arm');
+local p1_left_l_leg = piece('p1_left_l_leg');
+local p1_left_u_arm = piece('p1_left_u_arm');
+local p1_left_u_leg = piece('p1_left_u_leg');
+local p1_penis = piece('p1_penis');
+local p1_right_l_arm = piece('p1_right_l_arm');
+local p1_right_l_leg = piece('p1_right_l_leg');
+local p1_right_u_arm = piece('p1_right_u_arm');
+local p1_right_u_leg = piece('p1_right_u_leg');
+local p1_u_body = piece('p1_u_body');
+local p2_head = piece('p2_head');
+local p2_l_body = piece('p2_l_body');
+local p2_left_l_arm = piece('p2_left_l_arm');
+local p2_left_l_leg = piece('p2_left_l_leg');
+local p2_left_u_arm = piece('p2_left_u_arm');
+local p2_left_u_leg = piece('p2_left_u_leg');
+local p2_right_l_arm = piece('p2_right_l_arm');
+local p2_right_l_leg = piece('p2_right_l_leg');
+local p2_right_u_arm = piece('p2_right_u_arm');
+local p2_right_u_leg = piece('p2_right_u_leg');
+local p2_u_body = piece('p2_u_body');
+local scriptEnv = {	
+	Scene = Scene,
+	center = center,
+	p1_head = p1_head,
+	p1_l_body = p1_l_body,
+	p1_left_l_arm = p1_left_l_arm,
+	p1_left_l_leg = p1_left_l_leg,
+	p1_left_u_arm = p1_left_u_arm,
+	p1_left_u_leg = p1_left_u_leg,
+	p1_penis = p1_penis,
+	p1_right_l_arm = p1_right_l_arm,
+	p1_right_l_leg = p1_right_l_leg,
+	p1_right_u_arm = p1_right_u_arm,
+	p1_right_u_leg = p1_right_u_leg,
+	p1_u_body = p1_u_body,
+	p2_head = p2_head,
+	p2_l_body = p2_l_body,
+	p2_left_l_arm = p2_left_l_arm,
+	p2_left_l_leg = p2_left_l_leg,
+	p2_left_u_arm = p2_left_u_arm,
+	p2_left_u_leg = p2_left_u_leg,
+	p2_right_l_arm = p2_right_l_arm,
+	p2_right_l_leg = p2_right_l_leg,
+	p2_right_u_arm = p2_right_u_arm,
+	p2_right_u_leg = p2_right_u_leg,
+	p2_u_body = p2_u_body,
+	x_axis = x_axis,
+	y_axis = y_axis,
+	z_axis = z_axis,
+}
+
+local Animations = include('orgy_couple_ref_movement.lua')
+assert(Animations["REF_MOVEMENT"])
+assert(Animations["REF_MOVEMENT"][1])
+assert(Animations["REF_MOVEMENT"][1].time)
+assert(Animations["REF_MOVEMENT"][1].time == 1)
+assert(Animations["REF_MOVEMENT"][1].commands)
+assert(Animations["REF_MOVEMENT"][1].commands[1].c)
+assert(Animations["REF_MOVEMENT"][1].commands[1].p)
+assert(Animations["REF_MOVEMENT"][1].commands[1].a)
+assert(Animations["REF_MOVEMENT"][1].commands[1].t)
+assert(Animations["REF_MOVEMENT"][1].commands[1].s)
+
 
 -- center = piece "center"
 -- left = piece "left"
@@ -43,9 +84,9 @@ if not center then echo("Unit of type"..UnitDefs[Spring.GetUnitDefID(unitID)].na
 
 function script.Create()
     -- generatepiecesTableAndArrayCode(unitID, true)
-    TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
-	
-	-- setupAnimation()
+
+	setupAnimation()
+	Hide(p1_penis)
 
 	-- Spring.MoveCtrl.Enable(unitID,true)
 	-- x,y,z =Spring.GetUnitPosition(unitID)
@@ -53,38 +94,6 @@ function script.Create()
 	StartThread(AnimationTest)
 end
 
-function setupAnimation()
-    local map = Spring.GetUnitPieceMap(unitID);
-	local switchAxis = function(axis) 
-		if axis == z_axis then return y_axis end
-		if axis == y_axis then return z_axis end
-		return axis
-	end
-
-    local offsets = constructSkeleton(unitID, map.center, {0,0,0});
-    
-    for a,anim in pairs(Animations) do
-	if type(anim) == "table" then
-        for i,keyframe in pairs(anim) do
-            local commands = keyframe.commands;
-            for k,command in pairs(commands) do
-				assert(command.p)
-				if command.p and type(command.p)== "string" then
-					command.p = map[command.p]
-				end
-                -- commands are described in (c)ommand,(p)iece,(a)xis,(t)arget,(s)peed format
-                -- the t attribute needs to be adjusted for move commands from blender's absolute values
-                if (command.c == "move") then
-                    local adjusted =  command.t - (offsets[command.p][command.a]);
-                    Animations[a][i]['commands'][k].t = command.t - (offsets[command.p][command.a]);
-                end
-				
-			   -- Animations[a][i]['commands'][k].a = switchAxis(command.a)	
-            end
-        end
-    end
-    end
-end
 
 local animCmd = {['turn']=Turn,['move']=Move};
 
@@ -94,51 +103,14 @@ local axisSign ={
 	[z_axis]=1,
 }
 
-function PlayAnimation(animname, piecesToFilterOutTable, speed)
-	local speedFactor = speed or 1.0
-	if not piecesToFilterOutTable then piecesToFilterOutTable ={} end
-	assert(animname, "animation name is nil")
-	assert(type(animname)=="string", "Animname is not string "..toString(animname))
-	assert(Animations[animname], "No animation with name ")
-	
-	
-    local anim = Animations[animname];
-	local randoffset 
-    for i = 1, #anim do
-        local commands = anim[i].commands;
-        for j = 1,#commands do
-            local cmd = commands[j];
-			randoffset = 0.0
-			if cmd.r then
-				randVal = cmd.r* 100
-				randoffset = math.random(-randVal, randVal)/100
-			end
-			
-			if cmd.ru or cmd.rl then
-				randUpVal=	( cmd.ru or 0.01)*100
-				randLowVal=	( cmd.rl or 0	)*100
-				randoffset = math.random(randLowVal, randUpVal)/100
-			end
-			
-			if cmd.p and not piecesToFilterOutTable[cmd.p] then	
-				animCmd[cmd.c](cmd.p, cmd.a, axisSign[cmd.a] * (cmd.t + randoffset) ,cmd.s*speedFactor)
-				
-			end
-        end
-        if(i < #anim) then
-            local t = anim[i+1]['time'] - anim[i]['time'];
-            Sleep(t*33* math.abs(1/speedFactor)); -- sleep works on milliseconds
-        end
-    end
-end
 
 function constructSkeleton(unit, piece, offset)
+	echo("Enter constructSkeleton")
     if (offset == nil) then
         offset = {0,0,0};
     end
-	
-    local bones = {};
 
+    local bones = {};
     local info = Spring.GetUnitPieceInfo(unit,piece);
 
     for i=1,3 do
@@ -158,16 +130,91 @@ function constructSkeleton(unit, piece, offset)
             end
         end
     end        
+	echo("Leave constructSkeleton")
     return bones;
 end
 
-function AnimationTest()
-setupAnimation()
+function setupAnimation()
+	echo("setupAnimation")
+	
+	local switchAxis = function(axis) 
+		if axis == z_axis then return y_axis end
+		if axis == y_axis then return z_axis end
+		return axis
+	end
+	
+	assert(Spring.GetUnitPieceMap)
+    local map = Spring.GetUnitPieceMap(unitID);
+	assert(map)
+    local offsets = constructSkeleton(unitID, map.Scene, {0,0,0});
+    
+    for a,anim in pairs(Animations) do
+        for i,keyframe in pairs(anim) do
+            local commands = keyframe.commands;
+			assert(commands)
+            for k,command in pairs(commands) do
+			if command.p then
+                -- commands are described in (c)ommand,(p)iece,(a)xis,(t)arget,(s)peed format
+                -- the t attribute needs to be adjusted for move commands from blender's absolute values
+                if (command.c == "move") then
+                    local adjusted =  command.t - (offsets[command.p][command.a]);
+                    Animations[a][i]['commands'][k].t = command.t - (offsets[command.p][command.a]);
+                end
+				
+				if type(command.p) == "string" then
+				 Animations[a][i]['commands'][k].p = piece(command.p)
+				end
+				
+				Animations[a][i]['commands'][k].a = switchAxis(command.a)	
+				
+            end
+            end
+        end
+   end
+   echo("Leave setupAnimation")
+end
+            
+local animCmd = {['turn']=Spring.UnitScript.Turn,['move']= Spring.UnitScript.Move};
+function PlayAnimation(animname)
+    local anim = Animations[animname];
+	assert(anim)
+	assert(#anim>0)
+    for i = 1, #anim do
+        local commands = anim[i].commands;
+		assert(commands)
+        for j = 1,#commands do
+            local cmd = commands[j];
+			assert(cmd)
+			if cmd.c and cmd.p then
+				echo("Playing Animation")
+				if not animCmd[cmd.c] then echo("Animation has no command "..cmd.c) end
+				animCmd[cmd.c](cmd.p,cmd.a,cmd.t,cmd.s);
+			end
+        end
+        if(i < #anim) then
+            local t = anim[i+1]['time'] - anim[i]['time'];
+            Sleep(t*33); -- sleep works on milliseconds
+        end
+    end
+end
+--doggy
+--doggystanding
+        
+function FuckFest()
+resetAll(unitID)
+allKeys = {}
+for k,v in pairs(Animations) do
+	allKeys[#allKeys+1] = k
+end
+
+
 while true do
-	resetAll(unitID)
-	Sleep(1000)
-	PlayAnimation("REF_MOVEMENT",10)
-	Sleep(9000)
+	randTime = math.random(5,150)
+	Sleep(randTime)
+	Hide(center)
+	
+	PlayAnimation(allKeys[random(1,#allKeys)])
+	WaitForTurns(scriptEnv)
 	echo("Animation Test loop")
 	end
 end
--- scripts/orgy_couple_ref_movement.lua
+++ scripts/orgy_couple_ref_movement.lua
@@ -1,24 +1,21 @@
 return {
-['REF_MOVEMENT'] =
-{
-	{
-		['time'] = 1,
-		['commands'] = {
-			{['c']='turn',['p']=p1_left_u_arm, ['a']=x_axis, ['t']=0.000000, ['s']=0.000000},
-			{['c']='turn',['p']=p1_left_u_arm, ['a']=y_axis, ['t']=0.000000, ['s']=0.000000},
-			{['c']='turn',['p']=p1_left_u_arm, ['a']=z_axis, ['t']=1.560003, ['s']=3.107231},
-			{['c']='turn',['p']=p2_left_u_arm, ['a']=x_axis, ['t']=-0.000003, ['s']=0.000002},
-			{['c']='turn',['p']=p2_left_u_arm, ['a']=y_axis, ['t']=-1.626935, ['s']=3.118908},
-			{['c']='turn',['p']=p2_left_u_arm, ['a']=z_axis, ['t']=0.000003, ['s']=0.000004},
-			{['c']='turn',['p']=p2_right_u_arm, ['a']=x_axis, ['t']=-3.137806, ['s']=3.194784},
-			{['c']='turn',['p']=p2_right_u_arm, ['a']=y_axis, ['t']=-0.000000, ['s']=0.000000},
-			{['c']='turn',['p']=p2_right_u_arm, ['a']=z_axis, ['t']=0.000000, ['s']=0.000000},
-		}
-	},
-	{
-		['time'] = 30,
-		['commands'] = {
-		}
-	},
-}
+["REF_MOVEMENT"]={
+		{
+			['time'] = 1,
+			['commands'] = 
+			{
+				{c ='turn',p="p1_l_body", a=z_axis, t=1.570796, s=1.624962},					
+				{c='turn',p="p1_left_u_arm", a=y_axis, t=-1.570796, s=1.624962},
+				{c='turn',p="p1_right_u_arm", a=y_axis, t=-0.785398, s=0.812481},
+				{c='turn',p="p2_l_body", a=z_axis, t=-1.570796, s=1.624962},
+				{c='turn',p="p2_left_u_arm", a=z_axis, t=-0.785398, s=0.812481},
+				{c='turn',p="p2_right_u_arm", a=z_axis, t=0.785398, s=0.812481},
+			}
+		},
+		{
+			['time'] = 30,
+			['commands'] = {
+			}
+		},
+	}
 }
