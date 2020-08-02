35ed80ec8f2f07bef7ec485fba35ee785ed42d4d
Florian Seidl -Schulz <PicassoCT@gmx.de>
2020-08-02 23:35:34 +0200

    added widget - not working properly yet

--- LuaUI/widgets/cmd_snipeminigame.lua
+++ LuaUI/widgets/cmd_snipeminigame.lua
@@ -1,30 +1,34 @@
--------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------
+
+local versionNumber = "2.03"
 
 function widget:GetInfo()
   return {
-    name = "Nine Mens Morris",
-    desc = " Controll the raids icons nine men morris",
+    name = "Snipe Mini Game",
+    desc = "controlls the behaviour of raid icon minigame unitspawning",
     author = "dizekat",
     date = "Jan,2008",
     license = "GNU GPL, v2 or later",
-    layer = 5,
-    enabled = true,
-	hidden = true
+    layer = 0,
+    enabled = true
   }
 end
 
-
-
-function widget:GameStart()
+function widget:Update(dt)
 
 end
 
 local raidIconDefID = nil
 
 function widget:Initialize()
- 
+
+	WG['snipeminigame'] = {}
+    WG['snipeminigame'].testfunc = function()
+        return maxAlpha
+    end
+
+    return true
 end
+
 for k,v in pairs(UnitDefs) do
 	if v.name == "raidicon" then
 	raidIconDefID = k
@@ -47,14 +51,23 @@ end
 
 local raidIcon = UnitDefNames["raidicon"].id
 
+boolPlacementActive= false
 function widget:MousePress(x,y,button)
+	if (mButton ~= 1) then return false end
+
  local targType, targID = spTraceScreenRay(mx, my, false, inMinimap)
  Spring.Echo(targType.." - > "..targID)
-        if targType == 'unit' then
-            defID = Spring.GetUnitDefID(targID)
-			if defID == raidIcon then
+        if targType == 'unit' and raidIcons[targID] then
+			boolPlacementActive = true
 			Spring.Echo("Clicked on raid icon")
-			
-			end
+			return true 
 		end
+end
+
+ function widget:MouseMove(mx, my, dx, dy, mButton)
+ 
+ end
+
+function widget:MouseRelease(mx, my, mButton)
+
 end
