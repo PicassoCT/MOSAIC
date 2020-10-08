f1303f4fe1b1efd88fa023a0813177a7db4c5d3d
PicassoCT <PicassoCT@gmx.de>
2020-10-08 15:18:28 +0200

    Create smartDroneMine.lua
    I ve lost my coding mojo. I need honey comb rainbow alpha patterns on invisible units

---
+++ weapons/smartDroneMine.lua
@@ -0,0 +1,37 @@
+--- http://springrts.com/wiki/Weapon_Variables#Cannon_.28Plasma.29_Visuals
+local weaponName = "smartMineDrone" --this is the actually maschinegune of the inferno trooper
+local weaponDef = {
+    name = "exploding charge unit flying",
+    weaponType = [[MissileLauncher]],
+
+    damage = {
+        default = 1500
+    },
+	range = 1024,
+	impulseBoost            = 0,
+	impulseFactor = 0.4,
+	reloadtime = 10,
+    areaOfEffect = 256,
+	 noSelfDamage = true,
+	trajectoryHeight = 2.1 ,
+	avoidFeature            = false,
+	avoidGround = false,
+	smokeTrail = true,
+	startVelocity  = 380,
+	weaponAcceleration = 100,
+	turnRate = 150,
+	weaponVelocity = 450,
+	tracks = true,
+	flightTime = 14.5 ,
+	turret = true,
+	model = "smart_mine.s3o",
+	explosionScar = true, 
+    explosionGenerator = "custom:missile_explosion",
+    cegTag = "gunprojectile",
+    texture1 = "gunshot",
+	fireStarter  = 50.0,
+	cameraShake =1.0
+	
+    }
+  
+return lowerkeys({ [weaponName] = weaponDef })
