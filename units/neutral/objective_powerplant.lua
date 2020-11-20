d28046c03ffc72f66276a2c4fcba7d10b9f5b00b
PicassoCT <PicassoCT@gmx.de>
2020-11-20 23:42:02 +0100

    added unit descriptors

---
+++ units/neutral/objective_powerplant.lua
@@ -0,0 +1,52 @@
+local objective_powerplant = Building:New{
+	corpse					= "",
+	maxDamage        	= 1500,
+	mass           	= 500,
+	buildCostEnergy    	= 5,
+	buildCostMetal    	= 5,
+	explodeAs				= "none",
+	name = "Orbital Microwave  Collector Powerplant",
+	description = "provides power for major urban areas ( < 250 Mio people)",
+	buildPic = "house.png",
+	iconType = "house",
+	Builder					= false,
+	levelground				= true,
+	FootprintX = 8,
+	FootprintZ = 8,
+	script 					= "placeholderscript.lua",
+	objectName       	= "objective_powerplant.dae",
+
+	
+	
+	YardMap =     [[oooooooo
+                    oooooooo
+                    oooooooo
+                    oooooooo
+                    oooooooo
+                    oooooooo
+                    oooooooo
+                    oooooooo]]	, 
+	                    
+
+	customparams = {	
+		normaltex = "unittextures/house_arab_normal.dds",
+		helptext			= "Civilian Building",
+		baseclass			= "Building", -- TODO: hacks
+    },
+	
+	buildoptions = 
+	{
+	"civilian_arab0"
+	},
+	usepiececollisionvolumes = false,
+	collisionVolumeType = "box",
+	collisionvolumescales = "130 9000 130",
+	category = [[GROUND BUILDING]],
+
+}
+
+return lowerkeys({
+	--Temp
+	["objective_powerplant"] = objective_powerplant:New()
+	
+})
