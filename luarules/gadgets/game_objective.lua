baad052594c77772cbcc6991c8fa82abea08d248
PicassoCT <PicassoCT@gmx.de>
2020-11-28 00:46:53 +0100

    auto-place objective

--- luarules/gadgets/game_objective.lua
+++ luarules/gadgets/game_objective.lua
@@ -16,7 +16,7 @@ end
     VFS.Include("scripts/lib_Build.lua")
     VFS.Include("scripts/lib_mosaic.lua")
 
-
+gaiaTeamID = Spring.GetGaiaTeamID()
 objectiveTypes = getObjectiveTypes(UnitDefs)
 Objectives = {}
 DeadObjectives = {}
@@ -25,14 +25,50 @@ if (gadgetHandler:IsSyncedCode()) then
 GameConfig = getGameConfig()
 
  function gadget:Initialize()
- 	
+
+
+ 	mapCenter = {x=Game.mapSizeX/2, z= Game.mapSizeZ/2}
+
+		oz = math.min(Game.mapSizeX ,Game.mapSizeZ )- math.random(500, 1000)
+		ox= Game.mapSizeX/2
+		k=1
+	for i=1, 2 do
+		x,z = ox- mapCenter.x, oz - mapCenter.z
+		if x then
+			rx, rz= Rotate(x, z, math.rad(k*180+90))
+			k=k+1
+			rx, rz = rx + mapCenter.x, rz + mapCenter.z
+			h= Spring.GetGroundHeight(rx,rz)
+			if h then
+				filteredObjectives ={}
+				if h > 5 then 
+					 for id, medium in pairs (objectiveTypes) do
+					 	if medium == "land" then
+							filteredObjectives[id]= id
+						end
+					end
+
+
+				else 
+			 		for id, medium in pairs (objectiveTypes) do
+						if medium == "water" then
+							filteredObjectives[id]= id
+						end
+					end
+				end		
+				key,element = randDict(filteredObjectives)
+				id = Spring.CreateUnit(element,rx, h, rz, 1, gaiaTeamID )
+			end
+		end
+	end
  end
 
  function gadget:UnitCreated(UnitID, whatever)
  	local type=Spring.GetUnitDefID(UnitID);
-	if objectiveTypes[type] then
+	if objectiveTypes[type] and Spring.GetUnitTeam == gaiaTeamID then
 		x,y,z=Spring.GetUnitPosition(UnitID)
 		Objectives[UnitID] = {x= x, y=y, z=z}
+		Spring.SetUnitAlwaysVisible(UnitID,true)
 	end
  end
 
