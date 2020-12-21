d79f84fd2a6c57c026133925a46ae05196f4b56e
PicassoCT <PicassoCT@gmx.de>
2020-12-21 20:41:31 +0100

    VERSION{0.694}

--- scripts/interrogationIconScript.lua
+++ scripts/interrogationIconScript.lua
@@ -9,9 +9,6 @@ TablesOfPiecesGroups = {}
 center = piece"Center"
 Rotator = piece"Rotator"
 GameConfig = getGameConfig()
-function script.HitByWeapon(x, z, weaponDefID, damage)
-end
-
 
 function script.Create()
 	Spring.SetUnitAlwaysVisible(unitID,true)
@@ -25,126 +22,99 @@ function script.Create()
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
 
 	StartThread(interrogatePercentage)	
-
-end
-
-function getPieceWithNeigbhours(mapTable)
-
-randStart = math.random(#mapTable) 
-
-	for i= randStart, #mapTable do
-	curCursor = i
-		if mapTable[curCursor] == false then
-			dirDice = math.random(1,4)
-			
-			if dirDice == 1 and  mapTable[i -1] and mapTable[i -1] == true then return curCursor end
-			if dirDice == 2 and  mapTable[i +1] and mapTable[i +1] == true then return curCursor end
-			if dirDice == 3 and  mapTable[i -4] and mapTable[i -4 ] == true then return curCursor end
-			if dirDice == 4 and  mapTable[i + 4] and mapTable[i  +4 ] == true then return curCursor end
-		end
-	end
-	
-	for i= 1, randStart do
-	curCursor = i
-		if mapTable[curCursor] == false then
-			dirDice = math.random(1,4)
-			
-			if dirDice == 1 and  mapTable[i -1] and mapTable[i -1] == true then return curCursor end
-			if dirDice == 2 and  mapTable[i +1] and mapTable[i +1] == true then return curCursor end
-			if dirDice == 3 and  mapTable[i -4] and mapTable[i -4 ] == true then return curCursor end
-			if dirDice == 4 and  mapTable[i + 4] and mapTable[i  +4 ] == true then return curCursor end
-		end
-	end
-
-	for i= 1, #mapTable do
-	curCursor = i
-		if mapTable[curCursor] and mapTable[curCursor] == false then return curCursor end
-	end
 end
 
 function showTime()
+	StartThread(pulseLoad)
 	showAll()
 	hideT(TablesOfPiecesGroups["Load"])	
 	Spin(Rotator,y_axis,math.rad(42),15)
-		while true do
-			StartThread(visualizeProgress)
-			showT(TablesOfPiecesGroups["Load"], 1, 	math.max(1, #TablesOfPiecesGroups["Load"] * getCurrentPercent()))
-			Sleep(startCountDown)
-			for i=1,#TablesOfPiecesGroups["Ring"] do
-				val = math.random(2,15)*randSign()
-				speed= math.random(5,25)
-				Spin(TablesOfPiecesGroups["Ring"][i],y_axis, math.rad(val),speed)
-			end
-			Sleep(1)
+	while true do
+		showT(TablesOfPiecesGroups["Load"], 1, 	math.max(1, #TablesOfPiecesGroups["Load"] * getCurrentPercent()))
+		Sleep(OnePercent)
+		for i=1,#TablesOfPiecesGroups["Ring"] do
+			val = math.random(2,15)*randSign()
+			speed= math.random(5,25)
+			Spin(TablesOfPiecesGroups["Ring"][i],y_axis, math.rad(val),speed)
 		end
+		Sleep(1)
+	end
 end
 
 function getCurrentPercent()
-	return ( startCountDown / (startCountDown-countDown))
+	return ( (startCountDown-countDown)/startCountDown )
 end
 
 countDown = (GameConfig.InterrogationTimeInFrames/30)*1000
 startCountDown = countDown
 OnePercent = math.ceil(countDown /100)
 
+function pulseLoad()
+	counter= 0
+	while true do
+		frame = ((Spring.GetGameFrame() % 300) /300) *math.pi
+		counter= counter + 1
+		for i=1,#TablesOfPiecesGroups["Load"] do
+			value= math.sin( frame + (i +counter) * (math.pi)/#TablesOfPiecesGroups["Load"]) *50
+			if TablesOfPiecesGroups["Load"][i] then
+			Move(TablesOfPiecesGroups["Load"][i], y_axis,value  , 0)
+			end
+		end
+	Sleep(30)
+	end
+end
+
 function interrogatePercentage()
+	for i=1,#TablesOfPiecesGroups["Ring"] do
+		value= 32^i
+		WMove(TablesOfPiecesGroups["Ring"][i],z_axis,value,0)
+	end
+
+	for i=1,#TablesOfPiecesGroups["Ring"] do
+		syncMoveInTime(TablesOfPiecesGroups["Ring"][i], 0, 0, 0, 750)
+	end
+
 	timer = 0
 	StartThread(showTime)	
---[[	while not GG.raidIconDone or not GG.raidIconDone[unitID]  do
+	while not GG.raidIconDone or not GG.raidIconDone[unitID]  do
 		Sleep(100)
 		timer = timer + 100
 		if timer > 5000 then 
 			Spring.DestroyUnit(unitID,false, true)
 		end
-	end--]]
+	end
 
 	SetUnitValue(COB.WANT_CLOAK, 0)
 
 	while  countDown > 0 do --GG.raidPercentageToIcon and GG.raidPercentageToIcon[unitID] do
 		Sleep(OnePercent)
 		countDown = math.max(0, countDown - OnePercent)
-		Spring.Echo( "Interrogation running :" ..countDown)
+		Sleep(1)
 	end
+	for i=1,#TablesOfPiecesGroups["Ring"] do
+		value= 32^i
+		syncMoveInTime(TablesOfPiecesGroups["Ring"][i], 0, value, 0, 750)
+	end
+	WaitForMoves(TablesOfPiecesGroups["Ring"])
 
-	--GG.raidIconDone[unitID].boolInterogationComplete = true
-	--Spring.DestroyUnit(unitID,false, true)
+	GG.raidIconDone[unitID].boolInterogationComplete = true
+	Spring.DestroyUnit(unitID,false, true)
 end
 
 function visualizeProgress()
-		lastIndex= math.random(1,16)
-		alreadyVisitedTable=makeTable(false, 16)	
-		hideT(TablesOfPiecesGroups["puzzle"])
-		Show(TablesOfPiecesGroups["puzzle"][lastIndex])
-		alreadyVisitedTable[lastIndex] = true
-		factor = math.min(20,math.max(1, getCurrentPercent()* 20))
-		
-		while factor < 17 do
-			factor = factor +1
-
-			pieceWithNeighbour = getPieceWithNeigbhours(alreadyVisitedTable)
-				if pieceWithNeighbour and (TablesOfPiecesGroups["puzzle"][pieceWithNeighbour]) then
-					Show(TablesOfPiecesGroups["puzzle"][pieceWithNeighbour])
-					alreadyVisitedTable[pieceWithNeighbour] = true
-				end
-
-			Sleep(1000)
-		end
+		hideT(TablesOfPiecesGroups["Puzzle"])
+		showT(TablesOfPiecesGroups["Puzzle"],1, #TablesOfPiecesGroups["Puzzle"] * getCurrentPercent())		
 end
 
 function script.Killed(recentDamage, _)
     return 1
 end
 
-
-
-
 function script.Activate()
-
     return 1
 end
 
 function script.Deactivate()
-
     return 0
 end
 
