d6ed8eb814d76e32de06a5967da42c446862cf54
PicassoCT <PicassoCT@gmx.de>
2021-01-17 13:45:53 +0100

    added seperat weapon

---
+++ weapons/aamachinegun.lua
@@ -0,0 +1,43 @@
+
+local weaponName = "aamachinegun" 
+local weaponDef = {
+    name = "M27-64",
+    weaponType = [[Cannon]],
+    --damage
+    damage = {
+        default = 4,
+        HeavyArmor = 1,
+    },
+    areaOfEffect = 8,
+    explosionGenerator = "custom:gunimpact",
+    cegTag = "gunprojectile",
+    texture1 = "gunshot",
+
+    --physics
+    weaponVelocity = 850,
+    reloadtime = 5,
+    range = 700,
+    sprayAngle = 300,
+    tolerance = 8000,
+    lineOfSight = true,
+    turret = true,
+   
+	
+    craterMult = 0,
+    burst = 15,
+    burstrate = 0.2,
+    soundStart = "weapons/machinegun/salvo2.ogg",
+    soundtrigger = 1,
+    SweepFire = 1,
+    interceptor=1,
+    --apperance
+    rgbColor = [[0.95 0.5  0.2]],
+    size = 1.2,
+    stages = 20,
+    separation = 0.2,
+    collideFirebase = false,
+    avoidFriendly= false,
+
+}
+
+return lowerkeys({ [weaponName] = weaponDef })
