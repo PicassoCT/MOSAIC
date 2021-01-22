02710e99e9e8aff24f5d8a37a797c5ce16381fbf
PicassoCT <PicassoCT@gmx.de>
2021-01-22 14:49:24 +0100

    added sound

--- scripts/tankscript.lua
+++ scripts/tankscript.lua
@@ -13,6 +13,7 @@ center = piece "Body1"
 aimpiece = piece "Turret1"
 Cannon1 = piece "Cannon1"
 SIG_RESETAIM = 1
+myDefID = Spring.GetUnitDefID(unitID)
 
 function script.Create()
     generatepiecesTableAndArrayCode(unitID)
@@ -26,13 +27,10 @@ function script.Killed(recentDamage, _)
     return 1
 end
 
-
 --- -aimining & fire weapon
 function script.AimFromWeapon1()
     return aimpiece
 end
-
-
 
 function script.QueryWeapon1()
     return Cannon1
@@ -46,13 +44,14 @@ SetSignalMask(SIG_RESETAIM)
 boolNoLongerAiming= false
 Sleep(5000)
 	boolNoLongerAiming= true
+    StartThread( PlaySoundByUnitDefID , myDefID, "sounds/tank/rotate.ogg", 1.0, 1000, 1)
 	WTurn(aimpiece,2 ,0, 0.5)
 	WTurn(Cannon1,1,0,0.5)
 
 end
 function script.AimWeapon1(Heading, pitch)
     --aiming animation: instantly turn the gun towards the enemy
-
+    StartThread( PlaySoundByUnitDefID , myDefID, "sounds/tank/rotate.ogg", 0.5, 1000, 1)
 	StartThread(noLongerAiming)
 	WTurn(aimpiece,2 ,Heading, 0.7)
 	WTurn(Cannon1,1,-pitch,0.7)
@@ -66,9 +65,8 @@ function script.FireWeapon1()
 end
 
 
-
 function script.StartMoving()
--- Turn(aimpiece,y_axis , math.rad(0), math.pi)
+
 end
 
 function script.StopMoving()
