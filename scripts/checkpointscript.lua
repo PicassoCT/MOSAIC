83afd25882786ffbee46c5b0bac32c01b88f56aa
PicassoCT <PicassoCT@gmx.de>
2021-01-27 00:50:00 +0100

    created checkpoint script

--- scripts/checkpointscript.lua
+++ scripts/checkpointscript.lua
@@ -13,84 +13,94 @@ end
 -- left = piece "left"
 -- right = piece "right"
 -- aimpiece = piece "aimpiece"
+SIG_LIGHT = 1
 if not aimpiece then echo("Unit of type "..UnitDefs[Spring.GetUnitDefID(unitID)].name .. " has no aimpiece") end
 if not center then echo("Unit of type"..UnitDefs[Spring.GetUnitDefID(unitID)].name .. " has no center") end
 
-function script.Create()
-    -- generatepiecesTableAndArrayCode(unitID)
-    TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
-	-- Spring.MoveCtrl.Enable(unitID,true)
-	-- x,y,z =Spring.GetUnitPosition(unitID)
-	-- Spring.MoveCtrl.SetPosition(unitID, x,y+500,z)
-	-- StartThread(AnimationTest)
+function hideShowLamps(stage)
+	Signal(SIG_LIGHT)
+	SetSignalMask(SIG_LIGHT)
+
+	hideT(TablesOfPiecesGroups["Lamp"])
+	if stage == "off" then
+		Show(TablesOfPiecesGroups["Lamp"][1])	
+		Show(TablesOfPiecesGroups["Lamp"][4])	
+	elseif stage = "good" then
+		Show(TablesOfPiecesGroups["Lamp"][2])	
+		Show(TablesOfPiecesGroups["Lamp"][5])	
+	elseif stage == "bad" then
+		Show(TablesOfPiecesGroups["Lamp"][3])	
+		Show(TablesOfPiecesGroups["Lamp"][6])	
+	end
+	Sleep(3000)
+	hideT(TablesOfPiecesGroups["Lamp"])
+	Show(TablesOfPiecesGroups["Lamp"][1])	
+	Show(TablesOfPiecesGroups["Lamp"][4])	
 end
 
--- function AnimationTest()
--- while true do
-	-- resetAll(unitID)
-	-- Sleep(3000)
-		-- WTurn(right,z_axis,math.rad(-89),math.pi)
-		-- WTurn(left,z_axis,math.rad(89),math.pi)
-	-- Sleep(3000)
-	-- Turn(left,y_axis,math.rad(-89),math.pi)
-	-- WTurn(right,y_axis,math.rad(89),math.pi)
-	
-	-- Turn(left,y_axis,math.rad(89),math.pi)	
-	-- WTurn(right,y_axis,math.rad(-89),math.pi)
-	-- end
--- end
+function script.Create()
+    TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
+    StartThread(revelioThread)
+end
+
+boolActive = true
+function revelioThread()
+ StartThread(hideShowLamps,"off")
+local cachedNonInterest={}
+gaiaTeamID = Spring.GetGaiaTeamID()
+if not GGG.DisguiseCivilianFor then GG.DisguiseCivilianFor = {} end
+
+	while true do
+			if boolActive == true then
+			process(
+				 getAllNearUnit(unitID, gameConfig.checkPointRevealRange),
+				 function(id)
+				 	if not cachedNonInterest[id] then return id end
+				 end,
+				 function(id)
+				 	if spGetUnitTeam(id) ~= myTeamID then
+				 		return id
+				 	else
+						cachedNonInterest[id] = true
+				 	end
+				 end,
+				 function(id)
+				 	defID = spGetUnitDefID(id)
+				 	if defID and civilianTypeTable[defID] then
+				 		--if DisguiseCivilianFor	
+				 		if GG.DisguiseCivilianFor[id] then -- Disguised Unit
+				 			disguisedUnitID = GG.DisguiseCivilianFor[id] --make transparent
+ 							if not GG.OperativesDiscovered then  GG.OperativesDiscovered = {} end
+ 							 GG.OperativesDiscovered[disguisedUnitID] = true
+ 							 StartThread(hideShowLamps,"good")
+				 		else --pay a annoyanceFine
+							transferFromTeamToAllTeamsExceptAtUnit(id, 
+								 myTeamID,
+								 gameConfig.checkPointPropagandaCost, 
+								 {
+								 	[gaiaTeamID] = true
+								 }
+								 )
+							 StartThread(hideShowLamps,"bad")
+				 		end
+				 	end
+				 end
+
+				)
+			end
+		Sleep(250)
+	end
+end
 
 function script.Killed(recentDamage, _)
-
-    --createCorpseCUnitGeneric(recentDamage)
     return 1
 end
 
 
---- -aimining & fire weapon
--- function script.AimFromWeapon1()
-    -- return aimpiece
--- end
-
-
-
--- function script.QueryWeapon1()
-    -- return aimpiece
--- end
-
--- function script.AimWeapon1(Heading, pitch)
-    -- aiming animation: instantly turn the gun towards the enemy
-
-    -- return true
--- end
-
-
--- function script.FireWeapon1()
-
-    -- return true
--- end
-
-
-
-function script.StartMoving()
-end
-
-function script.StopMoving()
-end
-
 function script.Activate()
-
     return 1
 end
 
 function script.Deactivate()
-
     return 0
 end
-
--- function script.QueryBuildInfo()
-    -- return center
--- end
-
--- Spring.SetUnitNanoPieces(unitID, { center })
-
