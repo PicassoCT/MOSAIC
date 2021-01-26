b451662a2b3ea7a12f076a017dc5985aabfee3f8
PicassoCT <PicassoCT@gmx.de>
2021-01-27 00:37:03 +0100

    refactored the missile start trail out

---
+++ gamedata/explosions/cruisemissiletrail.lua
@@ -0,0 +1,55 @@
+-- dirt
+
+return {
+  ["cruisemissiletrail"] = {
+  
+  ashcloud={	
+				air=true,
+				class=[[CSimpleParticleSystem]],
+				count=1,
+				ground=true,
+				water=false,
+				
+				properties={
+				
+				texture=[[SmokeAshCloud]],
+				colormap           = [[
+				 0.95 0.35 0.03 0.9
+				 0.95 0.35 0.03 0.5 
+				 0.5 0.5 0.55 0.5 
+				 0.5 0.5 0.55 0.6 
+				 0.345 0.25 0.27 0.5
+				 0.5 0.36 0.36 0.75 
+				 0.08 0.08 0.08 0.75
+				 0 0 0 0.0001]],
+		
+
+				pos                = [[0 r-42 r42, 26r14, 0 r-42 r42]],
+				gravity            = [[0r-0.05r0.05, -0.1 , 0r-0.05r0.05]],
+				emitvector         = [[0, s1, 0]],
+				 emitrot            = 3,
+				emitrotspread      = 25,
+
+
+				sizeGrowth	= 0.45,
+				sizeMod		= 1.000000000001,
+
+		
+				airdrag			= 0.55,
+				particleLife		=60,
+				particleLifeSpread	= 35,
+				numParticles		= 4,
+				particleSpeed		= 0.09,
+				particleSpeedSpread	= 0.12,
+				particleSize		= 4.008,
+				particleSizeSpread	= 4,
+
+				directional		= true, 
+				useAirLos		= true,
+				},
+	
+		},
+	},
+
+}
+
