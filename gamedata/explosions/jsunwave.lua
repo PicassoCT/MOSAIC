b998cc1e53c5013b4c9841c63ad903c38f803b19
PicassoCT <PicassoCT@gmx.de>
2021-02-18 01:48:56 +0100

    trying to get the nuke going

---
+++ gamedata/explosions/jsunwave.lua
@@ -0,0 +1,28 @@
+-- missile_explosion
+
+return {
+	["jsunwave"] = {
+		
+		groundflash = {
+			air = true,
+			ground = true,
+			water = true,
+			alwaysvisible = true,
+			circlealpha = 3.0,
+			circlegrowth = 7,--6
+			flashalpha = 0.1,
+			flashsize = 210,
+			
+			ttl = 120,--53
+			
+			color = {
+				[1] = 1,
+				[2] = 0.6,
+				[3] = 0.12,
+			},
+		},
+		
+		
+	},
+	
+}
