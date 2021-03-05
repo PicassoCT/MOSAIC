3b29b4955f72fde724c1fd2011ab7d7f1c1f83ca
PicassoCT <PicassoCT@gmx.de>
2021-03-05 15:35:21 +0100

    VERSION{0.717}

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -6,7 +6,7 @@ function getGameConfig()
     return {
         instance = {
             culture = "arabic", -- "international", "western", "asia"
-            Version = "Alpha: 0.716"
+            Version = "Alpha: 0.717"
         },
 
         numberOfBuildings = math.ceil(75 * unitFactor),
