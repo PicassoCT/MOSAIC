3dc52fe4075af6e4e535833c786885b4ea1b47ea
PicassoCT <PicassoCT@gmx.de>
2021-03-15 21:15:10 +0100

    rewrote and tested the revealedGraph

--- luaui/widgets_mosaic/gui_RevealedGraph.lua
+++ luaui/widgets_mosaic/gui_RevealedGraph.lua
@@ -18,10 +18,11 @@ function widget:GetInfo()
     author    = "picasso",
     date      = "Year 2021 after Spring Died (A.S.D)",
     license   = "GNU GPL, v2 or later",
-    layer     = 5,
+    layer     = 15,
     enabled   = true
   }
 end
+
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -42,6 +43,7 @@ local glVertex               = gl.Vertex
 local spDiffTimers           = Spring.DiffTimers
 local spGetAllUnits          = Spring.GetAllUnits
 local spGetGroundNormal      = Spring.GetGroundNormal
+local spGetGroundHeight      = Spring.GetGroundHeight
 local spGetSelectedUnits     = Spring.GetSelectedUnits
 local spGetTeamColor         = Spring.GetTeamColor
 local spGetTimer             = Spring.GetTimer
@@ -54,6 +56,7 @@ local spGetUnitViewPosition  = Spring.GetUnitViewPosition
 local spIsUnitSelected       = Spring.IsUnitSelected
 local spIsUnitVisible        = Spring.IsUnitVisible
 local spSendCommands         = Spring.SendCommands
+local spGetGameFrame         = Spring.GetGameFrame
 
 
 --------------------------------------------------------------------------------
@@ -69,137 +72,160 @@ local Locations = {
   --if all revealed Units are no more, a location ceases to be relevant
 }
 
--- > Counts the number of elements in a dictionary
-function count(T)
-    if not T then return 0 end
-    local index = 0
-    for k, v in pairs(T) do if v then index = index + 1 end end
-    return index
-end 
-
--- >Retrieves a random element from a Dictionary
-function randDict(Dict)
-    if not Dict then return end
-    if lib_boolDebug == true then assert(type(Dict) == "table") end
-
-    countDict = count(Dict)
-    randElement = 1
-    if countDict > 1 then randElement = math.random(1, count(Dict)) end
-
-    index = 1
-    anyKey = 1
-    for k, v in pairs(Dict) do
-        anyKey = k
-        if index == randElement and k and v then return k, v end
-        index = inc(index)
-    end
-    
-    return anyKey, Dict[anyKey]
-end
-
-
-function addTestLocation()
+local function addTestLocation()
+  local locations = {}
   local allUnits = Spring.GetAllUnits()
-  local locationID = allUnits[math.random(1,#allUnits)]
-  local x,y,z = spGetUnitBasePosition(locationID)
+  local choiceIndex = math.random(1,#allUnits)
+  local locationID = allUnits[choiceIndex]
+  local coordinates = {}
+  coordinates.x,coordinates.y, coordinates.z = spGetUnitBasePosition(locationID)
   local revealedUnits = {}
-  
-  for i=1, i < math.random(3,6) do
-    dependent = randDict(allUnits)
-    revealedUnits[dependent]={
-    defID = spGetUnitDefID(dependent),
-    boolIsParent = math.random(0,1)==1
-    }
+  local upperBound = math.random(3,6)
+
+  local i = 1
+  local boolOneParentOnly= true
+  for i=1,  upperBound do
+    local rIndex = math.random(1,#allUnits)
+    local dependent = allUnits[rIndex]
+
+    if dependent then
+    revealedUnits[dependent]={}
+    revealedUnits[dependent].defID = spGetUnitDefID(dependent)
+      if boolOneParentOnly == false then   
+        revealedUnits[dependent].boolIsParent = false
+      else
+         boolOneParentOnly= false
+        revealedUnits[dependent].boolIsParent = true
+      end
+    end
   end
 
-  Locations[locationID]  = {
-    x=x,
-    y=y,
-    z=z,
-    teamID = spGetUnitTeam(locationID),
-    revealedUnits = revealedUnits
-  }
-
+  local n = #locations
+  locations[n + 1]  = {}
+  locations[#locations].radius = 50 --coordinates.x
+  locations[#locations].x = math.random(2000, 4000) --coordinates.x
+  locations[#locations].y = 0 --coordinates.y + 10
+  locations[#locations].z = math.random(2000, 4000) --coordinates.z
+  locations[#locations].teamID = spGetUnitTeam(locationID)
+  locations[#locations].revealedUnits = revealedUnits
+  return locations
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local teamColors = {}
-
-
 local locationLines  = 0
 local locationPolys  = 0
 local circleDivs   = 32
-local circleOffset = 0
-local heightOffset = 50
-
-local revealedLines = 0
-local revealedPolys = 0
+local circleOffset = 300
+local heightOffset = 0
 
 local startTimer = spGetTimer()
 local Polygon = { }
+local maxHeightIcon = 500
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-
-function newPolygon(x,y,z)
- local Polygon = {}
- addLineSegment(x,y,z)
-end
-
-function addLineSegment(x,y,z)
+local function addLineSegment(x,y,z)
   local pos = {[1]=x,[2]=y,[3]=z}
   Polygon[#Polygon+1] = pos
 end
 
-local polygonToDraw = {}
+local function newPolygon(x,y,z)
+     Polygon = {}
+      addLineSegment(x,y,z)
+end
 
-
-local function DrawLine(offsetY)
-  for i = 1, #polygonToDraw do
-    local x = polygonToDraw[i][1]
-    local z = polygonToDraw[i][3]
+local function DrawLine(polyToDraw, offsetY)
+  local n = #polyToDraw
+  for i = 1, n do
+    local x = polyToDraw[i][1]
+    local z = polyToDraw[i][3]
     local y = Spring.GetGroundHeight(x, z) + offsetY
     gl.Vertex(x,y,z)
   end
 end
 
 local function DrawRectangle( Locx, Locz, radius, offsetY)
-  local x = Locx - radius
-  local z = Locz + radius
-  local gh = Spring.GetGroundHeight(x, z) + offsetY
-  gl.Vertex(x,gh,z)
+  local gh = spGetGroundHeight(Locx, Locz)
+  local x = Locx - 2*radius
+  local z = Locz 
 
-  local x = Locx + radius
-  local z = Locz + radius
-  local gh = Spring.GetGroundHeight(x, z) + offsetY
-  gl.Vertex(x,gh,z)
+  gl.Vertex(x, gh + offsetY, z)
 
-  local x = Locx + radius
-  local z = Locz - radius
-  local gh = Spring.GetGroundHeight(x, z) + offsetY
-  gl.Vertex(x,gh,z)
+  local x = Locx 
+  local z = Locz 
+  gl.Vertex(x,gh + offsetY, z)
 
-  local x = Locx - radius
-  local z = Locz - radius
-  local gh = Spring.GetGroundHeight(x, z) + offsetY
+  local x = Locx 
+  local z = Locz - 2* radius
+  gl.Vertex(x,gh + offsetY, z)
+
+  local x = Locx - 2*radius
+  local z = Locz - 2*radius
+  gl.Vertex(x,gh + offsetY, z)
+
+  local x = Locx - 2*radius
+  local z = Locz 
+  gl.Vertex(x,gh + offsetY, z)
+
+  local x = Locx 
+  local z = Locz 
+  gl.Vertex(x,gh + offsetY, z) 
+
+  local x = Locx 
+  local z = Locz 
+
   gl.Vertex(x,gh,z)
 end
 
-function widget:GameFrame(n)
-    if n == startFrame + 60 then
-      for id,_ in pairs(Spring.GetAllUnits()) do
-          if math.random(0,1)== 1 then
+local function DrawTriangle( Locx, Locz, radius, offsetY)
+  local gh = spGetGroundHeight(Locx, Locz) 
+  local x = Locx - radius
+  local z = Locz 
 
+  gl.Vertex(x,gh + offsetY, z)
 
-          end
+  local x = Locx - radius*2
+  local z = Locz - radius
+  gl.Vertex(x,gh + offsetY,z)
 
-      end
+  local x = Locx 
+  local z = Locz -radius
+  gl.Vertex(x,gh + offsetY,z)
 
-    end
+  local x = Locx - radius
+  local z = Locz 
+  gl.Vertex(x,gh + offsetY,z)
+
+  local x = Locx 
+  local z = Locz 
+  gl.Vertex(x,gh +offsetY,z)
+
+  local x = Locx 
+  local z = Locz 
+  gl.Vertex(x,gh,z)
+end
+
+local function DrawCircle( Locx, Locz, radius, offsetY)
+  local gh = spGetGroundHeight(Locx, Locz) 
+
+  local radstep = (2.0 * math.pi) / 12
+  for i = 1, circleDivs do
+    local a = (i * radstep)
+    glVertex(Locx + math.sin(a) * radius, gh + offsetY, Locz + math.cos(a) * radius)
   end
 
-local startFrame  = Spring.GetGameFrame()
+  local x = Locx 
+  local z = Locz 
+  gl.Vertex(x,gh +offsetY,z)
+
+  local x = Locx 
+  local z = Locz 
+  gl.Vertex(x,gh,z)
+end
+
+local startFrame  = 1
 function widget:Initialize()
   startFrame  = Spring.GetGameFrame()
   locationLines = glCreateList(function()
@@ -214,62 +240,10 @@ function widget:Initialize()
       end
     end)
   end)
-  
-  --TODO Replace with Quadrants(Locations) and Triangles (Revealed Units)
-  locationPolys = glCreateList(function()
-    glBeginEnd(GL_TRIANGLE_FAN, function()
-   local     Triangle={
-              [1]={x=-1,y=0},
-              [2]={x=0,y=1},
-              [3]={x=1,y=0},
-            }
-
-      for i = 1, 3 do
-        glVertex(Triangle[1].x, heightOffset,Triangle[1].y)
-      end
-    end)
-  end)
-
-
-  revealedLines = glCreateList(function()
-    glBeginEnd(GL_LINE_LOOP, function()
-       local  Quad={
-              [1]={x=-1,y=0},
-              [2]={x=0,y=1},
-              [3]={x=1,y=0},
-              [4]={x=0,y=-1},
-          }
-    for i = 1, 4 do
-        glVertex(Quad[1].x, heightOffset , Quad[1].y)
-      end
-    end)
-  end)
-  
-  --TODO Replace with Quadrants(Locations) and Triangles (Revealed Units)
-  revealedPolys = glCreateList(function()
-    glBeginEnd(GL_TRIANGLE_FAN, function()
- local       Quad={
-    [1]={x=-1,y=0},
-    [2]={x=0,y=1},
-    [3]={x=1,y=0},
-    [4]={x=0,y=-1},
-}
-      for i = 1, 4 do
-        glVertex(Quad[1].x, heightOffset, Quad[1].y)
-      end
-    end)
-  end)
 end
-
 
 function widget:Shutdown()
-  glDeleteList(locationLines)
-  glDeleteList(locationPolys) 
-
-  glDeleteList(revealedLines)
-  glDeleteList(revealedPolys)
 end
-
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -302,9 +276,13 @@ local teamColors = {}
 
 local function GetTeamColorSet(teamID)
   if teamID == Spring.GetGaiaTeamID() then
-  r,g,b = 122/255, 250/255, 255/255
-    colors = {{ r, g, b, 0.4 },
-        { r, g, b, 0.7 }}
+  r,g,b = 27/255, 197/255, 184/255
+  dr,dg,db = 255/255, 191/255, 13/255
+    colors = {{ r, g, b, 0.7 }, --night
+              { r, g, b, 0.9 },
+              { dr, dg, db, 0.7 }, --day
+              { dr, dg, db, 0.9 }
+              }
   return colors  
   end
   
@@ -314,8 +292,11 @@ local function GetTeamColorSet(teamID)
   end
   local r,g,b = spGetTeamColor(teamID)
   
-  colors = {{ r, g, b, 0.4 },
-            { r, g, b, 0.7 }}
+  colors = {{ r, g, b, 0.8 },
+            { r, g, b, 0.9 },
+            { r, g, b, 0.8 },
+            { r, g, b, 0.9 }
+            }
   teamColors[teamID] = colors
   return colors
 end
@@ -386,9 +367,44 @@ function computeTeamColorOffsetByPlayer(teamID,hashUpper)
 
 end
 
-function isLocationOnScreen(x,y,z)
---TODO
-return true
+local function getDayTime()
+    local DAYLENGTH = 28800
+    local morningOffset = (DAYLENGTH / 2)
+    Frame = (spGetGameFrame() + morningOffset) % DAYLENGTH
+    local percent = Frame / DAYLENGTH
+    local hours = math.floor((Frame / DAYLENGTH) * 24)
+    local minutes = math.ceil((((Frame / DAYLENGTH) * 24) - hours) * 60)
+    local seconds = 60 - ((24 * 60 * 60 - (hours * 60 * 60) - (minutes * 60)) % 60)
+    return hours, minutes, seconds, percent
+end
+
+local function mirrorValue(val)
+    if val < 0.5 then
+        return val * 2    
+    else
+    return (0.5 - math.abs( val - 0.5))*2    
+    end
+end
+
+local function getDayTimeDependentColor(colSet)
+    local _,_,_, percent = getDayTime()
+    percent = 1-mirrorValue(percent)
+
+    local blendedColor = {}
+    blendedColor[1]={}
+
+     blendedColor[1][1] = (1-percent)* colSet[1][1] + (percent)* colSet[3][1]
+     blendedColor[1][2] = (1-percent)* colSet[1][2] + (percent)* colSet[3][2]
+     blendedColor[1][3] = (1-percent)* colSet[1][3] + (percent)* colSet[3][3]
+     blendedColor[1][4] = 0.7 
+       
+      blendedColor[2]={}  
+      blendedColor[2][1] = (1-percent)* colSet[2][1] + (percent)* colSet[4][1]
+      blendedColor[2][2] = (1-percent)* colSet[2][2] + (percent)* colSet[4][2]
+      blendedColor[2][3] = (1-percent)* colSet[2][3] + (percent)* colSet[4][3]
+      blendedColor[2][4] = 0.9
+      
+  return blendedColor
 end
 
 --------------------------------------------------------------------------------
@@ -397,21 +413,16 @@ end
 local unitCache={}
 local colorCache={}
 
-
-
 function widget:DrawWorld()
-  glLineWidth(3.0)
+  glLineWidth(1.0)
   glDepthTest(true)
   glPolygonOffset(-50, -2)
 
-  --local Locations = {
-  --contains with key unitID (x,y,z, teamID, radius and  revealedUnits as table[unitID] -> unitDefID
-  for unitID, LocationData in ipairs(Locations) do
-    local Loc = LocationData
-    if isLocationOnScreen(Loc.x, Loc.y, Loc.z) == true then
-      local teamID = Loc.teamID
+  for i=1, #Locations do
+  local Loc = Locations[i]
+       local teamID = Loc.teamID
       if teamID then
-        local radius = Loc.radius or 15
+        local radius = Loc.radius 
         local colorSet  = colorCache[teamID]
         if not colorSet then 
           colorSet= GetTeamColorSet(teamID)
@@ -419,37 +430,52 @@ function widget:DrawWorld()
         end
         --if there doesent exist yet a shifted teamcolourtable - compute it
         if not teamColorPlayers[teamID] then
-          computeTeamColorOffsetByPlayer(teamID,25)
+          computeTeamColorOffsetByPlayer(teamID, 25)
         end
+      --  Spring.Echo("gui_RevealedGraph:DrawWorld.glColor")
 
-        glColor(colorSet[1])
-        gl.LineWidth(6.0)
-        gl.BeginEnd(GL.LINE_STRIP, DrawRectangle, Loc.x, Loc.z, radius, heightOffset)
+      local dayTimeDependentColorSet = getDayTimeDependentColor(colorSet)
+        glColor(dayTimeDependentColorSet[1])
+        gl.LineWidth(3.0)
+        gl.BeginEnd(GL.LINE_STRIP, DrawRectangle, Loc.x, Loc.z, radius, maxHeightIcon)
         gl.LineWidth(1.0)
         gl.Color(1, 1, 1, 1)
 
-        for id, defID in ipairs(Loc.revealedUnits) do
-        local radius = GetUnitDefRealRadius(id)
-          glColor(colorSet[1])
-          glDrawListAtUnit(id, revealedPolys, false, radius, 1.0, radius)
-          glColor(colorSet[2])
-          glDrawListAtUnit(id, revealedLines, false, radius, 1.0, radius)
-          
+        local revealedUnits = Loc.revealedUnits
+        for id, defID in pairs(revealedUnits) do
+        local x, y, z = spGetUnitBasePosition(id)
+        local radius = GetUnitDefRealRadius(id) or 50
+        local gx, gy, gz = spGetGroundNormal(x, z)
+        local degrot = math.acos(gy) * 180 / math.pi
+
+        if Loc.boolIsParent then
+          glColor(dayTimeDependentColorSet[2])
+          gl.LineWidth(3.0)
+          gl.BeginEnd(GL.LINE_STRIP, DrawTriangle, x, z, radius, maxHeightIcon)
+          gl.LineWidth(1.0)
+          gl.Color(1, 1, 1, 1)
+
+        else
+          glColor(dayTimeDependentColorSet[2])
+          gl.LineWidth(3.0)
+          gl.BeginEnd(GL.LINE_STRIP, DrawCircle, x, z, radius, maxHeightIcon)
+          gl.LineWidth(1.0)
+          gl.Color(1, 1, 1, 1)
+        end
+
+        
           --drawStripe from Location to Unit
           newPolygon(Loc.x, Loc.y, Loc.z)
           ux,uy,uz = spGetUnitBasePosition(id)
           addLineSegment(ux,uy,uz)
-          gl.LineWidth(3.0)
-          gl.Color(colorSet[1].r,colorSet[1].g, colorSet[1].b, 0.25)
-          gl.BeginEnd(GL.LINE_STRIP, DrawLine, heightOffset)
+          gl.LineWidth(2.0)
+          glColor(dayTimeDependentColorSet[1])
+          gl.BeginEnd(GL.LINE_STRIP, DrawLine, Polygon ,maxHeightIcon-50)
           --reset
           gl.LineWidth(1.0)
           gl.Color(1, 1, 1, 1)
-
-        end
-      
+        end   
       end
-    end
   end
 
   glPolygonOffset(false)
@@ -457,14 +483,16 @@ function widget:DrawWorld()
   local alpha = 0.3
   glColor(1, 1, 1, alpha)
   glLineWidth(1.0)
+  -- Spring.Echo("gui_RevealedGraph:EndDrawWorld")
 end
 
 function widget:GameFrame(n)
-    if n % 10 == 0 then
-       addTestLocation()
+    if n % (150*3) == 0 then
+        Locations = addTestLocation()
         if GG.RevealedLocations then
-          -- Locations = GG.RevealedLocations
-        end 
+         Locations = GG.RevealedLocations
+         
+        end
     end
 
 end
