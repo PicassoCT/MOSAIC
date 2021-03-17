f4d681c7340c7f08cc6df4c87fc025a4e983306e
PicassoCT <PicassoCT@gmx.de>
2021-03-17 21:06:34 +0100

    added more decals

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -839,7 +839,8 @@ function getDecalMap(culture)
                 urban = {
                     "house_arab_decal1", "house_arab_decal2",
                     "house_arab_decal3", "house_arab_decal5",
-                    "house_arab_decal6", "house_arab_decal9","house_arab_decal16"
+                    "house_arab_decal6", "house_arab_decal9",
+                    "house_arab_decal16","house_arab_decal17"
                 }
             }
         }
