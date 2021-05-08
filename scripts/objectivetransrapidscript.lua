72559e86a7527b7ca4615bb9475155b21c19a773
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2021-05-08 23:57:06 +0200

    i should be sleeping

--- scripts/objectivetransrapidscript.lua
+++ scripts/objectivetransrapidscript.lua
@@ -13,23 +13,65 @@ local trainspeed = 500
 
 function script.Create()
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
+    hideT(TablesOfPiecesGroups["Tunnel1_"])
+    hideT(TablesOfPiecesGroups["Tunnel2_"])
+    hideT(TablesOfPiecesGroups["TunnelDetection"])
+    hideT(TablesOfPiecesGroups["Train"])
+    hideT(TablesOfPiecesGroups["Container"])
+
     StartThread(trainLoop, 1)
     StartThread(trainLoop, 2)
     rVal = math.random(0,360)
     Turn(center, y_axis, math.rad(rVal),0)
+    StartThread(deployTunnels, 1)
+    StartThread(deployTunnels, 2)
 end
 
+local boolOldState = false
+    function detectRisingEdge(boolActive)
+        boolResult = false
+        if boolOldState == false and boolActive == true then
+            boolResult = true
+        end
+
+        return boolResult
+    end
+
+    function detectFallingEdge(boolActive)
+        boolResult = false
+        if boolOldState == true and boolActive == false then
+            boolResult = true
+        end
+
+        return boolResult
+    end
 function deployTunnels(nr)
+detectionPiece = piece("TunnelDetection"..nr) 
+tunnelIndex = 1
+
+for i = maxDistanceTrain, -maxDistanceTrain, -50 do
+	WM(detectionPiece,trainAxis, i, 0)
+	boolAboveGround = isPieceAboveGround(unitID, pieceName, offset)
+	if detectRisingEdge(boolAboveGround) or detectFallingEdge(boolAboveGround) then
+		tunnelIndexPiece = piece("Tunnel"..nr.."_"..tunnelIndex)
+		if tunnelIndexPiece then
+		tunnelIndex = tunnelIndex + 1
+		WMove(tunnelIndexPiece, trainAxis, i, 0)
+		Show(tunnelIndexPiece)
+		end
+	end
 
 
+	boolOldState = boolAboveGround
+end
 end
 
 function buildTrain(nr)
-
+	Show(TablesOfPiecesGroups["Train"..nr])
 end
 
 function hideTrain(nr)
-
+	Hide(TablesOfPiecesGroups["Train"..nr])
 end
 
 function trainLoop(nr)
