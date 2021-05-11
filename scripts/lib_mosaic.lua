f5f89cf98d98c349e1036e0738c3440b1a15ac9b
Florian Seidl-Schulz <florian.seidl-schulz@sai-schweiger.de>
2021-05-12 00:41:32 +0200

    VERSION{0.727}

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -6,7 +6,7 @@ function getGameConfig()
     return {
         instance = {
             culture = "arabic", -- "international", "western", "asia"
-            Version = "Alpha: 0.726"
+            Version = "Alpha: 0.727"
         },
 
         numberOfBuildings = math.ceil(75 * unitFactor),
