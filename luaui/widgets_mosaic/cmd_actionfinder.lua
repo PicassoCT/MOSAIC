032d2f8955a8f098c14b27e9032670fb7e549315
PicassoCT <PicassoCT@gmx.de>
2021-05-20 10:45:12 +0200

    add senethrills action finder for testing

---
+++ luaui/widgets_mosaic/cmd_actionfinder.lua
@@ -0,0 +1,12 @@
+
+Orchid/Parrot Glidergun:
+
+Viral Adaptive Meme:
+Several Gigabyte sized Memem/Virus that can form terrorist cells
+
+Upload themselves/go dormant on hidden servers/ within existing servers.
+Download themselves as "update" or free-ware game.
+Apply behaviour tweaks, modifying the character of the user
+Users becomes addicted, dedicates his life to the cause/spread of the ParrotGlidergun.
+
+Large enough group "unlocks" hidden cause, results in the formation of a terrorist cell.
