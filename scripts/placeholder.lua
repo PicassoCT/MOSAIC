3f0728b0b1526da48ca5ac130fc9842fe8135b2f
PicassoCT <PicassoCT@gmx.de>
2021-06-26 15:13:48 +0200

    Placeholder reduced

--- scripts/placeholder.lua
+++ scripts/placeholder.lua
@@ -8,18 +8,6 @@ TablesOfPiecesGroups = {}
 
 function script.HitByWeapon(x, z, weaponDefID, damage) end
 
--- center = piece "center"
--- left = piece "left"
--- right = piece "right"
--- aimpiece = piece "aimpiece"
-if not aimpiece then
-    echo("Unit of type " .. UnitDefs[Spring.GetUnitDefID(unitID)].name ..
-             " has no aimpiece")
-end
-if not center then
-    echo("Unit of type" .. UnitDefs[Spring.GetUnitDefID(unitID)].name ..
-             " has no center")
-end
 
 function script.Create()
     -- generatepiecesTableAndArrayCode(unitID)
@@ -30,20 +18,6 @@ function script.Create()
     -- StartThread(AnimationTest)
 end
 
--- function AnimationTest()
--- while true do
--- resetAll(unitID)
--- Sleep(3000)
--- WTurn(right,z_axis,math.rad(-89),math.pi)
--- WTurn(left,z_axis,math.rad(89),math.pi)
--- Sleep(3000)
--- Turn(left,y_axis,math.rad(-89),math.pi)
--- WTurn(right,y_axis,math.rad(89),math.pi)
-
--- Turn(left,y_axis,math.rad(89),math.pi)	
--- WTurn(right,y_axis,math.rad(-89),math.pi)
--- end
--- end
 
 function script.Killed(recentDamage, _)
 
@@ -51,25 +25,7 @@ function script.Killed(recentDamage, _)
     return 1
 end
 
---- -aimining & fire weapon
--- function script.AimFromWeapon1()
--- return aimpiece
--- end
 
--- function script.QueryWeapon1()
--- return aimpiece
--- end
-
--- function script.AimWeapon1(Heading, pitch)
--- aiming animation: instantly turn the gun towards the enemy
-
--- return true
--- end
-
--- function script.FireWeapon1()
-
--- return true
--- end
 
 function script.StartMoving() end
 
