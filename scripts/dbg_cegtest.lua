03bbaf1a33c037a6cde1a11db7875228e1edf682
PicassoCT <PicassoCT@gmx.de>
2021-07-20 21:30:18 +0200

    added cegtest

---
+++ scripts/dbg_cegtest.lua
@@ -0,0 +1,137 @@
+include "createCorpse.lua"
+include "lib_OS.lua"
+include "lib_UnitScript.lua"
+include "lib_Animation.lua"
+
+include "lib_Build.lua"
+
+function cegFunction(x,y,z) 
+	cegname, sleeptime = getCegName()
+	Spring.SpawnCEG(getCegName(), x,  y + 100, z, math.random(-1,1),  math.random(-1,0),  math.random(-1,1), 60)
+	--spawnCegAtPiece(unitID, Quader04, cegname, -50, 0, 0, 0)
+	Sleep(sleeptime)
+end
+
+function script.HitByWeapon(x, z, weaponDefID, damage)
+end
+
+function getCegName()
+	echo("Cegspawn")
+	return "meteor", 5000
+end
+center = piece "center"
+Quader04 = piece "Quader04"
+Quader08 = piece "Quader08"
+Quader01 = piece "Quader01"
+
+function script.Create()
+	hideAll(unitID)
+	Spring.MoveCtrl.Enable(unitID,true)
+	x,y,z=Spring.GetUnitPosition(unitID)
+	Spring.MoveCtrl.SetPosition(unitID,x,y+150,z)
+	--generatepiecesTableAndArrayCode(unitID)
+	StartThread(saySay)
+	StartThread(emitSFX)
+	StartThread(hovertest)
+	StartThread(switchMove)
+	-- StartThread(testTurnInTime)
+	echo(minimalAbsoluteDistance(15,-15))
+	echo(minimalAbsoluteDistance(-15,-15))
+	echo(minimalAbsoluteDistance(-15,15))
+	echo(minimalAbsoluteDistance(360,-270))
+end
+Body= piece"dronetest"
+InnerWing= piece"power"
+HoverPoint = piece"HoverPoint"
+boolMoving=false
+function switchMove()
+	while true do
+
+	Sleep(100000)
+	boolMoving = true
+	end
+
+end
+
+function hovertest()
+	while true do
+		Turn(Body,x_axis,math.rad(math.random(-100,100)),0,true)
+		Sleep(1000)
+		hoverSegway(      center,
+						  Body,
+						 InnerWing, 
+						 HoverPoint,
+						 50, 
+						 -90,
+						 90,
+						 x_axis, 
+						 function(axis, p) return select(axis,Spring.UnitScript.GetPieceRotation(p)) end,
+						 function() return boolMoving end, 
+						 math.pi/10,
+						 math.pi
+						 )
+
+	Sleep(1000)
+	end
+end
+Kugel02 = piece"Kugel02"
+function script.Killed(recentDamage, _)
+	
+	createCorpseCUnitGeneric(recentDamage)
+	return 1
+end
+Quader02 = piece"Quader02"
+function testTurnInTime()
+	
+	while true do
+		reset(Quader02)
+		Sleep(1000)
+		turnInTime(Quader02, y_axis, 360, 5000, 0,0,0, false)
+		WaitForTurns(Quader02)	
+		turnInTime(Quader02, y_axis, -360, 5000, 0,360,0, false)
+		WaitForTurns(Quader02)	
+	end
+end
+
+function saySay()
+	while true do
+		Sleep(10000)
+		T = prepSpeach("Test 1 2 3 ", "Honk", 64, 0.5, 500)
+		
+		say(T, 5000, NameColour, { r = 1.0, g = 1.0, b = 1.0 }, OptionString, unitID)
+	end
+end
+
+function emitSFX()
+	--StartThread(constDistanceDrag)
+	StartThread(testTurnInTime)
+	x, y, z = Spring.GetUnitPosition(unitID)
+	i = 0
+	while true do
+	cegFunction(x,y,z)
+		
+	end
+end
+
+dragInRange = 1200
+liftUpRange = 900
+
+
+
+--- -aimining & fire weapon
+function script.AimFromWeapon1()
+	return Kugel02
+end
+
+
+
+function script.QueryWeapon1()
+	return Kugel02
+end
+
+function script.AimWeapon1(Heading, pitch)
+	--aiming animation: instantly turn the gun towards the enemy
+	WTurn(Kugel02,y_axis,Heading,0)
+	WTurn(Kugel02,x_axis,-pitch,0)
+	return true
+end
---
+++ units/neutral/cegTest.lua
@@ -0,0 +1,79 @@
+local unitName = "cegtest"
+
+local unitDef = {
+	name = "cegtestunit",
+	Description = "Testing the Cegs all day long",
+	objectName = "placeholder.s3o",
+	script = "dbg_cegtest.lua",
+	buildPic = "placeholder.png",
+	levelGround =false,
+	--cost
+	buildCostMetal = 15,
+	buildCostEnergy = 1,
+	buildTime = 1,
+	--Health
+	maxDamage = 6660,
+	idleAutoHeal = 15,
+	autoheal=10,
+	--Movement
+	mass=180020,
+	upRight=true,
+	blocking=false,
+	pushResistant=true,
+	Acceleration = 0.0000001,
+	BrakeRate = 0.0001,
+	FootprintX = 1,
+	FootprintZ = 1,
+	
+	
+	MaxSlope = 90,
+	MaxVelocity = 0.000001,
+	MaxWaterDepth = 55,
+	MovementClass = "TANK",
+	TurnRate = 1,
+	
+	sightDistance = 80,
+	
+	reclaimable=false,
+	Builder = false,
+	CanAttack = true,
+	CanGuard = false,
+	CanMove = true,
+	CanPatrol = false,
+	CanStop = true,
+	LeaveTracks = false,
+	useSmoothMesh = false,
+	
+
+	
+	customParams = {},
+	sfxtypes = {
+		explosiongenerators = {
+			"custom:glowingshrapnell",
+
+			
+		},
+		
+	},
+	
+	
+	
+	weapons = {
+
+		[1]={name = "slicergun",
+			
+			onlyTargetCategory = [[LAND]],
+		},
+			[2]={name = "jbeanstalkshield",
+		},
+
+		
+	},
+	Category = [[LAND]],
+	
+	
+	
+	
+}
+
+return lowerkeys({ [unitName] = unitDef })
