99ec12cd38c2273b0cf10e3478ca7a93003a1111
PicassoCT <PicassoCT@gmx.de>
2021-07-24 13:54:41 +0200

    removed debris

--- scripts/marketStand_arab_script.lua
+++ scripts/marketStand_arab_script.lua
@@ -23,12 +23,8 @@ function bodyBuilder()
 			end
 			)
 end
-counter = math.random(3,10)
+
 function script.HitByWeapon(x, z, weaponDefID, damage) 
-	if counter > 0 then 
-		counter = counter -1
-	Explode(TablesOfPiecesGroups["Prop"][math.random(1,#TablesOfPiecesGroups["Prop"])], SFX.FALL + SFX.NO_HEATCLOUD)
-	end
 return damage
 end
 
