4669c12a8fac96e0820ac128feaf57c06bd2dadd
PicassoCT <PicassoCT@gmx.de>
2021-09-03 10:58:36 +0200

    Update house_western.lua

--- units/neutral/house_western.lua
+++ units/neutral/house_western.lua
@@ -1,1 +1,49 @@
 
+local house_western = Building:New{
+	corpse					= "",
+	maxDamage        	= 3500,
+	mass           		= 500,
+	buildCostEnergy    	= 5,
+	buildCostMetal    	= 5,
+	explodeAs				= "none",
+	name = "Housing Block",
+	description = "houses civilians",
+	buildPic = "house.png",
+	iconType = "house",
+	Builder					= true,
+	levelground				= true,
+	FootprintX = 8,
+	FootprintZ = 8,
+	script 				= "house_europe_script.lua",
+	objectName       	= "house_europe.dae",
+
+	
+	
+		
+	YardMap =  [[yyyyyyyy
+				yyyyyyyy
+				yyyyyyyy
+				yyyyyyyy
+				yyyyyyyy
+				yyyyyyyy
+				yyyyyyyy
+				yyyyyyyy]]	,  
+	
+
+	customparams = {	
+		normaltex = "unittextures/house_europe_normal.dds",
+		helptext			= "Civilian Building",
+		baseclass			= "Building", -- TODO: hacks
+    },
+	
+	buildoptions = 
+	{
+	"civilian_arab0"
+	},
+	usepiececollisionvolumes = false,
+	collisionVolumeType = "box",
+	collisionvolumescales = "130 120 130",
+	category = [[GROUND BUILDING RAIDABLE]],
+
+}
+
