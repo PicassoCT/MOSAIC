905d72cd3a5c7509dc560957513689192b4df05a
PicassoCT <PicassoCT@gmx.de>
2021-09-04 00:41:23 +0200

    created civilian dummy for culture test

---
+++ units/neutral/Civilian_western0.lua
@@ -0,0 +1,27 @@
+local Civil = Civilian:New{
+	--This class inherits alot from its 
+	name = "Western Civilian",
+		corpse = "bodybag",
+	description = " innocent bystander <colateral>",
+	objectName        	= "civilian1_arab.dae",
+	
+	usepiececollisionvolumes = false,
+	collisionVolumeType = "box",
+	collisionvolumescales = "10 25 10",
+
+	buildPic = "civilian.png",
+	iconType = "civilian",
+	customparams = {
+		baseclass		= "Civilian", -- TODO: hacks
+		normaltex = "unittextures/arab_civilian_normal.dds",
+    },
+
+	
+	category = [[GROUND ARRESTABLE]],
+}
+
+
+return lowerkeys({
+	--Temp
+	["civilian_western0"] = Civil:New(),
+})
