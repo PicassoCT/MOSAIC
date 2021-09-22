d494ef5dd9b43b08b16958964ef816acf6f6a5b6
PicassoCT <PicassoCT@gmx.de>
2021-09-22 13:25:00 +0200

    Update Civilian_western2.lua

--- units/neutral/civilian_western/Civilian_western2.lua
+++ units/neutral/civilian_western/Civilian_western2.lua
@@ -1,1 +1,28 @@
 
+local Civil = Civilian:New{
+	--This class inherits alot from its 
+	name = "Western Civilian",
+		corpse = "bodybag",
+	description = " innocent bystander <colateral>",
+	objectName        	= "civilian2_western.dae",
+	
+	usepiececollisionvolumes = false,
+	collisionVolumeType = "box",
+	collisionvolumescales = "10 25 10",
+
+	buildPic = "civilian.png",
+	iconType = "civilian",
+	customparams = {
+		baseclass		= "Civilian", -- TODO: hacks
+		normaltex = "unittextures/arab_civilian_normal.dds",
+    },
+
+	
+	category = [[GROUND ARRESTABLE]],
+}
+
+
+return lowerkeys({
+	--Temp
+	["civilian_western2"] = Civil:New(),
+})
