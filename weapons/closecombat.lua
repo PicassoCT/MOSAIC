3d910620467a17f7ca920d4c63aae18b4f79ac84
PicassoCT <PicassoCT@gmx.de>
2021-11-29 23:26:32 +0100

    added politicians to bus

---
+++ weapons/closecombat.lua
@@ -0,0 +1,29 @@
+--- http://springrts.com/wiki/Weapon_Variables#Cannon_.28Plasma.29_Visuals
+local weaponName = "NOWEAPON"
+local weaponDef = {
+    name = "NoWeapon",
+    weaponType = [[Melee]],
+    --damage
+    damage = {
+        default = 0,
+        HeavyArmor = 0,
+    },
+
+    --physics
+    weaponVelocity = 450,
+    reloadtime = 1,
+    range = 15,
+    sprayAngle = 300,
+    tolerance = 8000,
+    turret = true,
+    craterMult = 0,
+
+    --  soundStart         = "",
+    --  soundtrigger=1,
+
+    --apperance
+    rgbColor = [[0 0 0]],
+    size = 0.1,
+}
+
+return lowerkeys({ [weaponName] = weaponDef })
