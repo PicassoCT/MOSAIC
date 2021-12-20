a441f5a672017ed44229c45dd1ffbee39d92a259
PicassoCT <PicassoCT@gmx.de>
2021-12-20 23:03:53 +0100

    VERSION{0.812}

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -35,7 +35,7 @@ function getGameConfig()
     return {
         instance = {
             culture = getInstanceCultureOrDefaultToo(getModOptionCulture() or GG.AllCultures.arabic), -- "international", "western", "asia", "arabic"
-            Version = "Alpha: 0.811" 
+            Version = "Alpha: 0.812" 
         },
 
         numberOfBuildings = math.ceil(150 * GG.unitFactor),
