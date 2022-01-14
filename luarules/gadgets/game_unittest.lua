b35a94747c194d53b6c88d306b439f21988b2633
PicassoCT <PicassoCT@gmx.de>
2022-01-15 00:44:36 +0100

    implemented a interrogation graph debugging test

---
+++ luarules/gadgets/game_unittest.lua
@@ -0,0 +1,143 @@
+function gadget:GetInfo()
+    return {
+        name = "Spawn TestUnits",
+        desc = "This gadget surveils memory conditions",
+        author = "Picasso",
+        date = "Sep. 2022",
+        license = "GNU GPL, v2 or later",
+        layer = 1,
+        enabled = true
+    }
+end
+
+if (gadgetHandler:IsSyncedCode()) then
+    VFS.Include("scripts/lib_OS.lua")
+    VFS.Include("scripts/lib_UnitScript.lua")
+    VFS.Include("scripts/lib_Animation.lua")
+    VFS.Include("scripts/lib_Build.lua")
+    VFS.Include("scripts/lib_mosaic.lua")
+
+    startFrame = Spring.GetGameFrame()+1
+    startTestAfterSeconds = 5
+    --Optimization
+    gaiaTeamID = Spring.GetGaiaTeamID()
+    GameConfig = getGameConfig()
+    houseTypeTable = getCultureUnitModelNames(GameConfig.instance.culture, "house", UnitDefs)
+
+    function gadget:Initialize()
+        initalizeInheritanceManagement()
+        startFrame = Spring.GetGameFrame()+1
+    end
+
+    function spawnTestNetwork()
+          T = Spring.GetTeamList ()
+            local nrOfTeams = #T
+            result = {}
+            playerTeam, aiTeam = "",""
+            Spring.Echo("nr of teams "..nrOfTeams)
+            for i = 1, nrOfTeams do
+                echo(Spring.GetTeamInfo(T[i])  )      
+                teamID, leader,  isDead,  isAiTeam, side,  allyTeam,  incomeMultiplier,  customTeamKeys =   Spring.GetTeamInfo (  T[i] )
+                if teamID ~= gaiaTeamID and not isAiTeam then
+                    playerTeam = T[i]
+                end
+
+                if teamID ~= gaiaTeamID and isAiTeam then
+                    aiTeam = T[i]
+                end
+            end
+
+    boolFirst = false
+    boolSecond = false
+    spawnedSafeHouse = {}
+    spawnedOperatives = {}
+    allreadyUsedHouses= {}
+    boolJustOnce = false
+    houses = process(Spring.GetAllUnits(),
+            function(id)
+                if Spring.GetUnitTeam(id) == gaiaTeamID and houseTypeTable[Spring.GetUnitDefID(id)] then
+                    if not boolJustOnce then
+                        boolJustOnce = true
+                        trainedOperative = createUnitAtUnit(aiTeam, "operativeinvestigator", id, math.random(-40,40), 0 ,  math.random(-40,40), nil, 1)
+                        registerFather(aiTeam, trainedOperative)
+                        spawnedOperatives[trainedOperative]=trainedOperative
+                        echo("Spawn Intial Test order")
+                        safehouseID = createUnitAtUnit(aiTeam, "antagonsafehouse", id, 0, 0 , 0, trainedOperative , 1)
+                        spawnedSafeHouse[safehouseID]=spawnedSafeHouse
+                        registerFather(aiTeam, safehouseID)
+                        registerChild(aiTeam, trainedOperative, safehouseID)
+                        allreadyUsedHouses[id] = id
+                        return
+                    end
+                    
+                    return id                    
+                end
+            end
+            )
+
+    size = 10
+
+        process(houses,            
+            function (id)
+                if not allreadyUsedHouses[id] and size > 0 then
+                    allreadyUsedHouses[id] = id
+                    size = size - 1
+                     echo("Spawn Test setup "..size)
+                    spawnHouseID=randDict(spawnedSafeHouse)
+                    trainedOperative = createUnitAtUnit(aiTeam, "operativeinvestigator", id, math.random(-40,40), 0 ,  math.random(-40,40), spawnHouseID, 1)
+                    spawnedOperatives[trainedOperative]=trainedOperative
+                    registerFather(aiTeam, spawnHouseID)
+                    registerChild(aiTeam, spawnHouseID, trainedOperative)
+
+                    trainedOperative = createUnitAtUnit(aiTeam, "operativeinvestigator", id,  math.random(-40,40), 0 ,  math.random(-40,40), spawnHouseID, 1)
+                    spawnedOperatives[trainedOperative]=trainedOperative
+                    registerFather(aiTeam, spawnHouseID)
+                    registerChild(aiTeam, spawnHouseID, trainedOperative)
+
+                    operativeCreatingNextSafeHouse = randDict(spawnedOperatives)
+                    safehouseID = createUnitAtUnit(aiTeam, "antagonsafehouse", id, 0, 0 , 0, operativeCreatingNextSafeHouse , 1)
+                    spawnedSafeHouse[safehouseID]=spawnedSafeHouse
+                    registerFather(aiTeam, operativeCreatingNextSafeHouse)
+                    registerChild(aiTeam, operativeCreatingNextSafeHouse, safehouseID)
+                end
+            end
+            )
+    end
+    
+
+boolOnce = true
+    function gadget:GameFrame(frame)
+
+        if frame > (startFrame + (startTestAfterSeconds*30)) and boolOnce == true then
+                    Spring.Echo("Game:Unit_Test:SpawningTestUnits")
+            spawnTestNetwork()
+            boolOnce = false
+        end
+        
+        if boolOnce and frame % 10 == 0 then
+            Spring.Echo( "Starting in "..math.abs(frame - ( startFrame + (startTestAfterSeconds*30))))
+        end
+
+        if frame % 90 == 0 and boolOnce == true then
+            process(spawnedSafeHouse,
+                    function(id)
+                        if doesUnitExistAlive(id) == true then return id end
+                    end,
+                    function (id)
+                        spawnCegAtUnit(id,"greenlight", 0, 50, 0)
+                    end
+                    )
+        end
+
+        if frame % 60 == 0 and boolOnce == true then
+            process(spawnedOperatives,
+                    function(id)
+                        if doesUnitExistAlive(id) == true then return id end
+                    end,
+                    function (id)
+                        spawnCegAtUnit(id,"redlight", 0, 50, 0)
+                    end
+                    )
+        end
+    end
+end
