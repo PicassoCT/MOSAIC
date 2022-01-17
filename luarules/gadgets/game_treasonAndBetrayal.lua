fffebf6ed883d0b6110bdacec6b8eb4ed5b294e7
PicassoCT <PicassoCT@gmx.de>
2022-01-17 23:11:30 +0100

    added - on attack by own team and death- dead drop game functionality

--- luarules/gadgets/game_treasonAndBetrayal.lua
+++ luarules/gadgets/game_treasonAndBetrayal.lua
@@ -19,11 +19,15 @@ if (gadgetHandler:IsSyncedCode()) then
     VFS.Include("scripts/lib_mosaic.lua")
 
     local spGetPosition = Spring.GetUnitPosition
+    local spGetUnitDefID = Spring.GetUnitDefID
+    local spGetUnitTeam = Spring.GetUnitTeam
+
     UnitDefNames = getUnitDefNames(UnitDefs)
     GameConfig = getGameConfig()
 
     MobileCivilianDefIds = getMobileCivilianDefIDTypeTable(UnitDefs)
     InterrogateableType = getInterrogateAbleTypeTable(UnitDefs)
+    operativeTypeTable = getOperativeTypeTable(UnitDefs)
     reruitmentDefID = UnitDefNames["recruitcivilian"].id
 
     gaiaTeamID = Spring.GetGaiaTeamID()
@@ -34,22 +38,65 @@ if (gadgetHandler:IsSyncedCode()) then
             -- Spring.Echo("UnitCreated of InterrogateableType")
             if father and doesUnitExistAlive(father) then
                 registerChild(unitTeam, father, unitid)
+                Spring.Echo("Interrogatable Unit created - child of "..father)
             else
                 registerFather(unitTeam, unitid)
+                Spring.Echo("Interrogatable Unit created - fatherless")
             end
-            -- Spring.Echo("UnitCreated Registered InterrogateableType")
+
         end
     end
 
+    function getFairDropPointNear(unitDead, teamID)
+       AllOperatives  =  process(Spring.GetAllUnits(),
+                        function(id)
+                                    if operativeTypeTable[spGetUnitDefID(id)] then
+                                        return id
+                                    end
+                                end
+                                )
+       maxdistance = 0
+       midpos= {x = 0, y = 0, z = 0}
+
+       for id in pairs(AllOperatives) do
+            for ad in pairs(AllOperatives) do
+                if id ~= ad and spGetUnitTeam(id) ~= spGetUnitTeam(ad) then
+                    if distanceUnitToUnit(id,ad) > maxdistance then
+                        maxdistance =  distanceUnitToUnit(id,ad) 
+                        a.x,a.y,a.z = spGetUnitPosition(id)
+                        b.x,b.y,b.z = spGetUnitPosition(ad)
+                        midpos.x, midpos.y, midpos.z = getMidPoint(a, b)
+                    end
+                end
+            end
+       end
+
+       if maxdistance == 0 then
+         x,y,z = Game.mapSizeX*(math.random(100,900)/1000), 0, Game.mapSizeZ*(math.random(100,900)/1000)
+         return x,y,z
+       end
+
+        return midpos.x, midpos.y, midpos.z 
+    end
+
     function gadget:UnitDestroyed(unitID, unitDefID, teamID, attackerID)
-        if InterrogateableType[unitDefID] then removeUnit(teamID, unitID) end
+        if InterrogateableType[unitDefID]  then
+            if attackerID and spGetUnitTeam(attackerID) ~= teamID then 
+                x,y,z = getFairDropPointNear(unitID)
+                copyID = Spring.CreateUnit("deaddrop", gaiaTeamID, x,y,z, 1 )
+                transferHierarchy(teamID, originalID, copyID)
+            else
+                removeUnit(teamID, unitID) 
+            end
+        end         
     end
 
     function gadget:Initialize()
         Spring.Echo(GetInfo().name .. " Initialization ended")
-        if not GG.OperativesDiscovered then GG.OperativesDiscovered = {} end
+        if not GG.OperativesDiscovered then 
+            GG.OperativesDiscovered = {} 
+        end
         initalizeInheritanceManagement()
         Spring.Echo(GetInfo().name .. " Initialization started")
     end
-
 end
