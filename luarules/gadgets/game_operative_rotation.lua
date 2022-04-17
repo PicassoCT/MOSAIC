0c7af6386d0113910a8c9f5b9515a0559f37ca81
PicassoCT <PicassoCT@gmx.de>
2022-04-18 00:21:23 +0200

    VERSION{0.850}

---
+++ luarules/gadgets/game_operative_rotation.lua
@@ -0,0 +1,53 @@
+function gadget:GetInfo()
+    return {
+        name = "game Snipe Mini Game",
+        desc = "This gadget handles the minigame",
+        author = "",
+        date = "Sep. 2008",
+        license = "GNU GPL, v2 or later",
+        layer = 0,
+        enabled = true
+    }
+end
+
+if (gadgetHandler:IsSyncedCode()) then
+    VFS.Include("scripts/lib_OS.lua")
+    VFS.Include("scripts/lib_UnitScript.lua")
+    VFS.Include("scripts/lib_mosaic.lua")
+
+    -- variables
+
+    local spGetUnitPosition = Spring.GetUnitPosition
+    
+
+    local spSetUnitAlwaysVisible = Spring.SetUnitAlwaysVisible
+    local spGetUnitDefID = Spring.GetUnitDefID
+    local spDestroyUnit = Spring.DestroyUnit
+    local postRoundTimeInSeconds = 15
+    local spGetUnitRotation = Spring.GetUnitRotation
+    local spSetUnitRotation = Spring.SetUnitRotation
+
+    function rotateUnitTowardsPoint(id, positionT)
+        x,y,z = spGetUnitPosition(id)
+        yaw, pitch, roll = spGetUnitRotation(id)
+        pitch = math.pi - math.atan2(x-positionT.x, z-positionT.z) 
+        spSetUnitRotation( id,  yaw,  pitch,  roll ) 
+    end
+
+    function gadget:RecvLuaMsg(msg, playerID)
+        if msg and string.find(msg, "OPROTPOS") then
+            T = split(msg, "|")
+            position = {}
+            position.x = tonumber(T[2])
+            position.y = tonumber(T[3])
+            position.z = tonumber(T[4])
+            for i = 5, #T do
+                local unitID = T[i]
+                if string.len(unitID) > 0 then
+                    rotateUnitTowardsPoint(tonumber(unitID), position)
+                end
+            end
+        end
+    end
+
+end 
---
+++ luaui/widgets_mosaic/cmd_operatorRotate.lua
@@ -0,0 +1,128 @@
+local versionNumber = "2.03"
+
+function widget:GetInfo()
+    return 
+{        name = "operatorRotate",
+        desc = "rotates operators towards the mouse curser when selected",
+        author = "picassoct",
+        date = "Jan,2008",
+        license = "GNU GPL, v2 or later",
+        layer = 0,
+        enabled = true,
+        hidden = true,
+        handler = true
+    }
+end
+
+local operatorTypeTable = {}
+local spTraceScreenRay = Spring.TraceScreenRay
+local spIsAboveMiniMap = Spring.IsAboveMiniMap
+local spGetSelectedUnits = Spring.GetSelectedUnits
+local raidIcons = {}
+local oldValues = {x= 0, z = 0}
+local operativeAssetDefID = 0
+local operativeInvestigatorDefID = 0
+local operativePropagatorDefID = 0
+local OPTIONS = {   -- these will be loaded when switching style, but the style will overwrite the those values
+
+    teamcolorOpacity                = 0.55,     -- how much teamcolor used for the base platter
+
+    -- opacity
+    spotterOpacity                  = 0.95,
+    baseOpacity                     = 0.15,     -- setting to 0: wont be rendered
+
+    -- animation
+    selectionStartAnimation         = true,
+    selectionStartAnimationTime     = 0.05,
+    selectionStartAnimationScale    = 0.82,
+    -- selectionStartAnimationScale = 1.17,
+    selectionEndAnimation           = true,
+    selectionEndAnimationTime       = 0.07,
+    selectionEndAnimationScale      = 0.9,
+    --selectionEndAnimationScale    = 1.17,
+}
+
+function widget:Initialize()
+    for k, v in pairs(UnitDefs) do
+        if string.find(v.name,"operative") then
+            operatorTypeTable[k] = v
+        end
+        if v.name == "operativepropagator" then
+            operativePropagatorDefID = k
+        end
+       if v.name == "operativeinvestigator" then
+            operativeInvestigatorDefID = k
+        end 
+        if v.name == "operativeasset" then
+            operativeAssetDefID = k
+        end         
+    end
+
+return true
+end
+
+
+local selChangedSec = 0
+local selectedUnitsSorted = Spring.GetSelectedUnitsSorted()
+local selectedUnitsCount = Spring.GetSelectedUnitsCount()
+function widget:SelectionChanged(sel)
+    checkSelectionChanges = true
+   -- Spring.Echo("Selection changed")
+end
+
+local   function serializeUnitIDTable(unitIDT)
+            local retString =""
+            if unitIDT then
+                for i=1,#unitIDT do
+                    retString = retString.. unitIDT[i].."|"
+                end
+            end
+            return retString
+        end
+
+function widget:Update(dt)
+    currentClock = os.clock()
+    maxSelectTime = currentClock - OPTIONS.selectionStartAnimationTime
+    maxDeselectedTime = currentClock - OPTIONS.selectionEndAnimationTime
+
+    selChangedSec = selChangedSec + dt
+    if checkSelectionChanges and selChangedSec >= 0.05 then
+        selChangedSec = 0
+        selectedUnitsSorted = Spring.GetSelectedUnitsSorted()
+        selectedUnitsCount = Spring.GetSelectedUnitsCount()
+        checkSelectionChanges = false
+    end
+
+    local boolContainsOperators = selectedUnitsSorted[operativeAssetDefID] or selectedUnitsSorted[operativePropagatorDefID]  or selectedUnitsSorted[operativeInvestigatorDefID] 
+    --check if selectedUnitsContainOperatives
+    if boolContainsOperators and selectedUnitsCount == 1 then
+       -- Spring.Echo("Selection contains Operators")
+        -- Trace screen ray
+        -- rotate them towards target
+        local mouseX, mouseY = Spring.GetMouseState()
+        local inMinimap = spIsAboveMiniMap(mouseX, mouseY)
+        local targType, targID = spTraceScreenRay(mouseX, mouseY, true, inMinimap, false, false, 50)
+        if targID then
+         Spring.SendLuaRulesMsg("OPROTPOS|" .. targID[1] .. "|" .. targID[2] .. "|" .. targID[3].."|"..
+            serializeUnitIDTable(selectedUnitsSorted[operativeAssetDefID]).. "|" ..
+            serializeUnitIDTable(selectedUnitsSorted[operativePropagatorDefID]).. "|" ..
+            serializeUnitIDTable(selectedUnitsSorted[operativeInvestigatorDefID]).. "|" )
+        end
+     end
+end
+
+function widget:GetConfigData(data)
+    savedTable = {}
+    savedTable.spotterOpacity                   = OPTIONS.spotterOpacity
+    savedTable.baseOpacity                      = OPTIONS.baseOpacity
+    savedTable.teamcolorOpacity                 = OPTIONS.teamcolorOpacity
+
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+    OPTIONS.spotterOpacity              = data.spotterOpacity           or OPTIONS.spotterOpacity
+    OPTIONS.baseOpacity             = data.baseOpacity              or OPTIONS.baseOpacity
+    OPTIONS.teamcolorOpacity            = data.teamcolorOpacity         or OPTIONS.teamcolorOpacity
+end
+
