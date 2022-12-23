fe64f0c01be7b02f4b596ad45ec8c9475c5784f3
PicassoCT <PicassoCT@gmx.de>
2022-12-23 07:40:14 +0100

    no

--- luarules/gadgets/gfx_neonHolograms.lua
+++ luarules/gadgets/gfx_neonHolograms.lua
@@ -23,14 +23,29 @@ if (gadgetHandler:IsSyncedCode()) then
     end
 
 else -- unsynced
-    local neonUnitTables = {}
     local glUseShader = gl.UseShader
     local glUniform = gl.Uniform
+    local glCopyToTexture = gl.CopyToTexture
+    local glUseShader = gl.UseShader
+    local glGetUniformLocation = gl.GetUniformLocation
+    local glUnitRaw = gl.UnitRaw
+    local glBlending = gl.Blending
+    local glScale = gl.Scale
     local startTimer = Spring.GetTimer()
     local shaderProgram = {}
-    local glGetUniformLocation = gl.GetUniformLocation
+    local vsx, vsy = 1600, 1200
+    local screencopy
     local diffTime = 0
 
+    local GL_SRC_ALPHA           = GL.SRC_ALPHA
+    local GL_ONE                 = GL.ONE
+    local GL_ZERO                = GL.ZERO
+    local GL_ONE_MINUS_SRC_ALPHA = GL.ONE_MINUS_SRC_ALPHA
+    local resxLocation = nil
+    local resyLocation = nil
+    local radius = 16
+    local resolution = 128
+    local neonUnitTables = {}
     local vertexShader = 
     [[
         #version 150 compatibility
@@ -39,80 +54,84 @@ else -- unsynced
 		varying vec2 vTexCoord;
 		varying vec3 vPosition;
 
-		attribute vec3 normal;
-        uniform mat3 normalMatrix;
-		uniform float time;
+		uniform sampler2D screencopy;
+        uniform float resolution;
+        uniform float radius;
+        uniform vec2 dir;
+        uniform float time;
 
         void main() {
             vNormal = normalize(normalMatrix * normal);
 			float normalYDistortion = sin(time);
             gl_Position = gl_Vertex;
 			gl_Position.xy = gl_Position.xy * vNormal.xy * (0.8 + (abs(sin(time))*0.2)*cos(time*2)); // Make the hologram waver in non heightdimension from time to time 
-			vPosition´= gl_Position;
+			vPosition = gl_Position.xyz;
         }
     ]]
 	
 fragmentshader =[[
-//---------------------------------------------------------------------------
-#version 420 core
-// fragment shader
-//https://stackoverflow.com/questions/64837705/opengl-blurring
-//"in" attributes from our vertex shader
-varying vec4 vColor;
-varying vec2 vTexCoord;
-varying vec3 vPosition;
+    //---------------------------------------------------------------------------
+    #version 150 compatibility
+    // fragment shader
+    //https://stackoverflow.com/questions/64837705/opengl-blurring
+    //"in" attributes from our vertex shader
+    varying vec4 vColor;
+    varying vec2 vTexCoord;
+    varying vec3 vPosition;
+    varying vec3 vNormal;
 
-//declare uniforms
-uniform sampler2D screencopy;
-uniform float resolution;
-uniform float radius;
-uniform vec2 dir;
-uniform float time;
+    //declare uniforms
+    uniform sampler2D screencopy;
+    uniform float resolution;
+    uniform float radius;
+    uniform vec2 dir;
+    uniform float time;
 
-void main() {
-    //this will be our RGBA sum
-    vec4 sum = vec4(0.0);
-    
-    //our original texcoord for this fragment
-    vec2 tc = vTexCoord;
-    
-    //the amount to blur, i.e. how far off center to sample from 
-    //1.0 -> blur by one pixel
-    //2.0 -> blur by two pixels, etc.
-    float blur = radius/resolution; 
-    
-    //the direction of our blur
-    //(1.0, 0.0) -> x-axis blur
-    //(0.0, 1.0) -> y-axis blur
-    float hstep = dir.x;
-    float vstep = dir.y;
-    //vec2 texCoord = vec2(pixelx * int(gl_TexCoord[0].x / pixelx), pixely * int(gl_TexCoord[0].y / pixely));
-	//vec4 origColor = texture2D(screencopy, texCoord);
-		
-    //apply blurring, using a 9-tap filter with predefined gaussian weights
-    
-    sum += texture2D(screencopy, vec2(tc.x - 4.0*blur*hstep, tc.y - 4.0*blur*vstep)) * 0.0162162162;
-    sum += texture2D(screencopy, vec2(tc.x - 3.0*blur*hstep, tc.y - 3.0*blur*vstep)) * 0.0540540541;
-    sum += texture2D(screencopy, vec2(tc.x - 2.0*blur*hstep, tc.y - 2.0*blur*vstep)) * 0.1216216216;
-    sum += texture2D(screencopy, vec2(tc.x - 1.0*blur*hstep, tc.y - 1.0*blur*vstep)) * 0.1945945946;
-    
-    sum += texture2D(screencopy, vec2(tc.x, tc.y)) * 0.2270270270;
-    
-    sum += texture2D(screencopy, vec2(tc.x + 1.0*blur*hstep, tc.y + 1.0*blur*vstep)) * 0.1945945946;
-    sum += texture2D(screencopy, vec2(tc.x + 2.0*blur*hstep, tc.y + 2.0*blur*vstep)) * 0.1216216216;
-    sum += texture2D(screencopy, vec2(tc.x + 3.0*blur*hstep, tc.y + 3.0*blur*vstep)) * 0.0540540541;
-    sum += texture2D(screencopy, vec2(tc.x + 4.0*blur*hstep, tc.y + 4.0*blur*vstep)) * 0.0162162162;
+    void main() {
+        //this will be our RGBA sum
+        vec4 sum = vec4(0.0);
+        
+        //our original texcoord for this fragment
+        vec2 tc = vTexCoord;
+        
+        //the amount to blur, i.e. how far off center to sample from 
+        //1.0 -> blur by one pixel
+        //2.0 -> blur by two pixels, etc.
+        float blur = radius/resolution; 
+        
+        //the direction of our blur
+        //(1.0, 0.0) -> x-axis blur
+        //(0.0, 1.0) -> y-axis blur
+        float hstep = dir.x;
+        float vstep = dir.y;
+        //vec2 texCoord = vec2(pixelx * int(gl_TexCoord[0].x / pixelx), pixely * int(gl_TexCoord[0].y / pixely));
+    	//vec4 origColor = texture2D(screencopy, texCoord);
+    		
+        //apply blurring, using a 9-tap filter with predefined gaussian weights
+        
+        sum += texture2D(screencopy, vec2(tc.x - 4.0*blur*hstep, tc.y - 4.0*blur*vstep)) * 0.0162162162;
+        sum += texture2D(screencopy, vec2(tc.x - 3.0*blur*hstep, tc.y - 3.0*blur*vstep)) * 0.0540540541;
+        sum += texture2D(screencopy, vec2(tc.x - 2.0*blur*hstep, tc.y - 2.0*blur*vstep)) * 0.1216216216;
+        sum += texture2D(screencopy, vec2(tc.x - 1.0*blur*hstep, tc.y - 1.0*blur*vstep)) * 0.1945945946;
+        
+        sum += texture2D(screencopy, vec2(tc.x, tc.y)) * 0.2270270270;
+        
+        sum += texture2D(screencopy, vec2(tc.x + 1.0*blur*hstep, tc.y + 1.0*blur*vstep)) * 0.1945945946;
+        sum += texture2D(screencopy, vec2(tc.x + 2.0*blur*hstep, tc.y + 2.0*blur*vstep)) * 0.1216216216;
+        sum += texture2D(screencopy, vec2(tc.x + 3.0*blur*hstep, tc.y + 3.0*blur*vstep)) * 0.0540540541;
+        sum += texture2D(screencopy, vec2(tc.x + 4.0*blur*hstep, tc.y + 4.0*blur*vstep)) * 0.0162162162;
 
-     //gl_FragColor = vColor * sum;
-	 //Transparency 
-	 float hologramTransparency = 0.5 - sin(time + vPosition.z) * 0.25 - sin(2*time)*0.1;
-	 float averageShadow = (fNormal.x*fNormal.x+fNormal.y*fNormal.y+fNormal.z+fNormal.z)/4.0;	
-	 gl_FragColor= vec4((gl_FragColor * (1.0-averageShadow)).xyz, gl_FragColor.z * hologramTransparency);
-	 
-}
-//---------------------------------------------------------------------------
-]]
+         //gl_FragColor = vColor * sum;
+    	 //Transparency 
+    	 float hologramTransparency = 0.5 - sin(time + vPosition.z) * 0.25 - sin(2*time)*0.1;
+    	 float averageShadow = (vNormal.x*vNormal.x+vNormal.y*vNormal.y+vNormal.z+vNormal.z)/4.0;	
+    	 gl_FragColor= vec4((gl_FragColor * (1.0-averageShadow)).xyz, gl_FragColor.z * hologramTransparency);
+    	 
+    }
+    //---------------------------------------------------------------------------
+    ]]
     local uniformInt = {
+          screencopy = 0,
           resx = vsx,
           resy = vsy
         }
@@ -132,26 +151,23 @@ void main() {
       uniformFloat  = uniformFloat,
 	  uniforms      = uniformTable
     }    
-local screencopy
-function gadget:ViewResize(viewSizeX, viewSizeY) --TODO test/assert
-	vsx, vsy = viewSizeX, viewSizeY
 
-screencopy = gl.CreateTexture(vsx, vsy, {
-    border = false,
-    min_filter = GL.NEAREST,
-    mag_filter = GL.NEAREST,
-	})
-end
 
+    function gadget:ViewResize(viewSizeX, viewSizeY) --TODO test/assert
+    	vsx, vsy = viewSizeX, viewSizeY
+
+    screencopy = gl.CreateTexture(vsx, vsy, {
+        border = false,
+        min_filter = GL.NEAREST,
+        mag_filter = GL.NEAREST,
+    	})
+    end
 
     local function setUnitNeonLuaDraw(callname, unitID, typeDefID)
         neonUnitTables[unitID] = typeDefID
         Spring.UnitRendering.SetUnitLuaDraw(unitID, true)
-    end
-	
-	local resxLocation = nil
-	local resyLocation = nil
-	local resolution = 128
+    end	
+
     function gadget:Initialize() 
 		vsx, vsy = gadgetHandler:GetViewSizes()
 		gadget:ViewResize(vsx, vsy)
@@ -171,6 +187,7 @@ end
                 resxLocation = glGetUniformLocation(shaderProgram, "resx")
                 resyLocation = glGetUniformLocation(shaderProgram, "resy")
                 resolution = glGetUniformLocation(shaderProgram, "resolution")
+                radius = glGetUniformLocation(shaderProgram, "radius")
             end
         else
             Spring.Echo("<Neon Shader>: GLSL not supported.")
@@ -181,25 +198,17 @@ end
         end
     end
 
-    local glUnitRaw = gl.UnitRaw
-    local glBlending = gl.Blending
-    local glScale = gl.Scale
-    local glUseShader = gl.UseShader
-    local GL_SRC_ALPHA           = GL.SRC_ALPHA
-    local GL_ONE                 = GL.ONE
-    local GL_ZERO                = GL.ZERO
-    local GL_ONE_MINUS_SRC_ALPHA = GL.ONE_MINUS_SRC_ALPHA
-	function gadget:GameFrame()
-	  glCopyToTexture(screencopy, 0, 0, 0, 0, vsx, vsy)
-	end
+    function gadget:DrawScreenEffects()
+      glCopyToTexture(screencopy, 0, 0, 0, 0, vsx, vsy)
+    end
+
     function gadget:DrawUnit(unitID, drawMode)
         if drawMode == 1 and neonUnitTables[unitID] then --normalDraw
             glUseShader(shaderProgram)
-            glBlending(GL_SRC_ALPHA, GL_ONE)
+            --glBlending(GL_SRC_ALPHA, GL_ONE)
             glUnitRaw(unitID, true)
-            glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+           -- glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
             glUseShader(0)
-            return true
         end       
     end
 
