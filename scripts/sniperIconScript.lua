cb16fe2a03c65f9b3a75dcc5d91d42f8204ca9ef
Florian Seidl-Schulz <picassoCT@gmx.de>
2023-11-28 13:11:34 +0100

    add sniperpos

--- scripts/sniperIconScript.lua
+++ scripts/sniperIconScript.lua
@@ -2,29 +2,52 @@ include "createCorpse.lua"
 include "lib_OS.lua"
 include "lib_UnitScript.lua"
 include "lib_Animation.lua"
---include "lib_Build.lua"
+
 
 TablesOfPiecesGroups = {}
 myDefID = Spring.GetUnitDefID(unitID)
-function script.HitByWeapon(x, z, weaponDefID, damage) end
-
+storePassengerID = nil
 attachPoint = piece("attachPoint")
 Icon = piece("Icon")
+
+function script.HitByWeapon(x, z, weaponDefID, damage) 
+	if storePassengerID then
+		Spring.AddDamage(storePassengerID
+		Spring.UnitDetach(storePassengerID)
+		Spring.DestroyUnit(storePassengerID, true, false)
+	end
+end
+
+
 function script.Create()
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
     Hide(attachPoint)
+	StartThread(threadStarter)
+end
+
+function threadStarter()
+	while true do
+		while storePassengerID do
+			if doesUnitExistAlive(storePassengerID) then
+				
+				
+			end
+		Sleep(100)
+		end
+	Sleep(1000)
+	end
+
 end
 
 
 function script.Killed(recentDamage, _)
-
-    -- createCorpseCUnitGeneric(recentDamage)
     return 1
 end
 
 function script.TransportPickup(passengerID)
     if passengerID then
        Spring.UnitAttach(unitID, passengerID, attachPoint)
+	   storePassengerID = passengerID
     end
 end
 
@@ -34,4 +57,5 @@ function script.Deactivate() return 0 end
 
 function script.TransportDrop(passengerID, x, y, z)
         Spring.UnitDetach(passengerID)
+		storePassengerID = nil
 end
