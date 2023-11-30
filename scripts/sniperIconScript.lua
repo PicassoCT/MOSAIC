9f0d75f844bddbf1d9dc68627039182067e7399a
PicassoCT <PicassoCT@gmx.de>
2023-11-30 23:34:39 +0100

    add: sniper code

--- scripts/sniperIconScript.lua
+++ scripts/sniperIconScript.lua
@@ -3,13 +3,15 @@ include "lib_OS.lua"
 include "lib_UnitScript.lua"
 include "lib_Animation.lua"
 
-
-TablesOfPiecesGroups = {}
-myDefID = Spring.GetUnitDefID(unitID)
-storePassengerID = nil
-attachPoint = piece("attachPoint")
-Icon = piece("Icon")
-
+local GameConfig = getGameConfig()
+local TablesOfPiecesGroups = {}
+local myDefID = Spring.GetUnitDefID(unitID)
+local storePassengerID = nil
+local attachPoint = piece("attachPoint")
+local Icon = piece("Icon")
+local gaiaTeamID = Spring.GetGaiaTeamID()
+local houseTypeTable = getHouseTypeTable(Unitdefs)
+local ux,uy,uz = Spring.GetUnitPosition(unitID)
 function script.HitByWeapon(x, z, weaponDefID, damage) 
 	if storePassengerID then
 		Spring.AddDamage(storePassengerID)
@@ -17,34 +19,57 @@ function script.HitByWeapon(x, z, weaponDefID, damage)
 		Spring.DestroyUnit(storePassengerID, true, false)
 	end
 end
+blockSize = GameConfig.houseSizeX/6
+halfBlockSize = blockSize/2
+function inHouse(id)
+    hx,hy,hz = Spring.GetUnitPosition(id)
+    minXLimit, maxXLimit = ux - (2.5*blockSize) ,ux + (2.5*blockSize) 
+    minZLimit, maxZLimit = uz - (2.5*blockSize) ,uz + (2.5*blockSize) 
+    if hx > minXLimit - halfsize and hx < minXLimit + halfsize  and
+       hz > minZLimit - halfsize and hz < minZLimit + halfsize  then
 
+       if  hx < minXLimit + halfsize and hx > minXLimit - halfsize  and
+            hz < minZLimit + halfsize and hz > minZLimit - halfsize  then
+            return false
+       end
+       return true
+    end
+    return false
+end
 
 function script.Create()
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
-	Spring.SetUnitNoSelect(unitID, true)
     Hide(attachPoint)
 	StartThread(threadStarter)
 end
 function checkIsWithinBuilding()
-	groundMask = TODO_getGroundMask
-	if not Spring.Bitwise.And(groundMask, 8) then
-		Spring.DestroyUnit(unitID, true, false)
+    boolOnAtLeastOneRoof= false
+    ux,uy,uz = Spring.GetUnitPosition(unitID)
+	foreach( 
+            getAllNearUnit(unitID, GameConfig.houseSizeX, gaiaTeamID),
+            function(id)
+                defID= Spring.GetUnitDefID(id)
+                if houseTypeTable[defID]then 
+                    if inHouse(id) then
+                        boolOnAtLeastOneRoof = true
+                    end
+                    return id
+                end
+            end
+            )
+        if not boolOnAtLeastOneRoof then
 		TransportDrop(storePassengerID)
-	end
-	
-	foreach( getAllNearUnit(unitID),
-			function(id)
-				TODO
-			end)
-	
-
+        Spring.DestroyUnit(unitID, true, false)
+        end
 end
-
+	
 function handleCommandTransfer(passengerID)
-
---TODO copy from car
+  while doesUnitExistAlive(passengerID) == true do
+        transferAttackOrder(unitID, passengerID)
+        transferStates(unitID, passengerID)
+        Sleep(100)
+    end
 end
-
 
 function threadStarter()
 	while true do
@@ -81,6 +106,7 @@ end
 
 function script.TransportPickup(passengerID)
     if passengerID then
+        Spring.SetUnitNoSelect(passengerID, true)
        Spring.UnitAttach(unitID, passengerID, attachPoint)
 	   setEnvironmentFireAllowance(true)
 	   storePassengerID = passengerID
@@ -92,6 +118,7 @@ function script.Activate() return 1 end
 function script.Deactivate() return 0 end
 
 function script.TransportDrop(passengerID, x, y, z)
+        Spring.SetUnitNoSelect(passengerID, false)
         Spring.UnitDetach(passengerID)
 		setEnvironmentFireAllowance(false)
 		storePassengerID = nil
