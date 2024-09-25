87a5f35fbba4ab1341d2d45741377a94efd3f1cd
PicassoCT <PicassoCT@gmx.dd>
2024-09-25 19:50:08 +0200

    first draft

--- scripts/objective_spaceportscript.lua
+++ scripts/objective_spaceportscript.lua
@@ -5,38 +5,20 @@ include "lib_Animation.lua"
 --include "lib_Build.lua"
 
 TablesOfPiecesGroups = {}
+Rocket=""
+BoosterT = {}
+Flames = {}
+Fusion = {}
+
 myDefID = Spring.GetUnitDefID(unitID)
 function script.HitByWeapon(x, z, weaponDefID, damage) end
-
-padDistance = 450
-x,y,z = Spring.GetUnitPosition(unitID)
-isSeaUnit = 0 > Spring.GetGroundHeight(x,z)
-Ground = piece("Ground")
-Water = piece("Water")
 function script.Create()
     echo(UnitDefs[myDefID].name.."has placeholder script called")
     -- generatepiecesTableAndArrayCode(unitID)
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
     hideAll(unitID)
-    Show(Water)
 
-    showT(TablesOfPiecesGroups["DroneShip"])
-    showT(TablesOfPiecesGroups["Capsule"])
-    showT(TablesOfPiecesGroups["Gauntry"])
-    if isSeaUnit then
-    else
-        Show(Ground)
-        showT(TablesOfPiecesGroups["Chains"])
-    end
-    StartThread(behaviourLoop)
 end
---Booster1
---Booster2
---Booster2
---SpaceX_Falcon_Heavy001
-FalconX = piece("SpaceX_Falcon_Heavy001")
-upaxis = y_axis
-boosterArrived = {}
 function landBooster( boosterNr, booster, rocket)
     boosterArrived[boosterNr] = false
     WMove(booster, upaxis, 9000, 0)
@@ -51,66 +33,46 @@ function landBooster( boosterNr, booster, rocket)
     boosterArrived[boosterNr] = true
 end
 
-function behaviourLoop()
-    while true do
-        boosterArrived = {}
-        for i=1, 3 do
-            StartThread(landBooster, i, piece("Booster"..i), piece("RocketRotator00"..i))
-        end
-        Sleep(500)
-
-        while not boosterArrived[1] or 
-              not boosterArrived[2] or 
-              not boosterArrived[3] do
-              Sleep(100)
-        end 
-        Move(TablesOfPiecesGroups["DroneShip"][1],z_axis, -padDistance, 25)
-        Move(TablesOfPiecesGroups["DroneShip"][2],z_axis, padDistance, 25)
-        Move(TablesOfPiecesGroups["DroneShip"][3],x_axis, padDistance, 25)
-        WaitForMoves(TablesOfPiecesGroups["DroneShip"])
-        Show(FalconX)
-        hideT(TablesOfPiecesGroups["Booster"])
-        Move(TablesOfPiecesGroups["DroneShip"][1],z_axis, 0, 25)
-        Move(TablesOfPiecesGroups["DroneShip"][2],z_axis, 0, 25)
-        WMove(TablesOfPiecesGroups["DroneShip"][3],x_axis, padDistance*2, 25)
-        Sleep(10000)
-        for i=1,9000, 100 do
-            WMove(FalconX, upaxis, i, 100*((i+1)/100))
-        end
-        Hide(FalconX)
-        reset(FalconX)
-        WMove(TablesOfPiecesGroups["DroneShip"][3],x_axis, 0, 25)
-        Sleep(5000)
-    end
-end
-
-
 function script.Killed(recentDamage, _)
-
-    -- createCorpseCUnitGeneric(recentDamage)
     return 1
 end
 
+function plattFormFireBloom()
+    Show(PlatFormCentralBloom)
+    while boolLaunching do
+        val = math.random(10, 77)*randSign()
+        Spin(PlatFormCentralBloom,y_axis, math.rad(val),0)
+        shift = 360/#TablesOfPiecesGroups["CyclesOfFire"]
+        for i=1, #TablesOfPiecesGroups["CyclesOfFire"] do
+            cycle = TablesOfPiecesGroups["CyclesOfFire"][i]
+            Turn(cycle,y_axis, math.rad(rotation),0) 
+            startVal = 90 + randSign()*90
+            Turn(cycle,x_axis, math.rad(startVal),360) --reset
+            Spin(cycle, x_axis, math.rad(val), fireBloomSpeed)
+        end
+        Sleep(9000)
+    end
+    StopSpins(TablesOfPiecesGroups["CyclesOfFire"])
+    hideT(TablesOfPiecesGroups["CyclesOfFire"])
+end
 
+function liftRocketShowStage(distanceUp, timeUp, cloud, spinValue, startValue)
+    Show(cloud)
+    Spin(cloud,y_axis, math.rad(spinValue), startValue)
+    moveInTime(Rocket, y_axis, distanceUp, timeUp)
+    WaitForMoves(Rocket)
+end
 
-function script.StartMoving() end
-
-function script.StopMoving() end
-
-function script.Activate() return 1 end
-
-function script.Deactivate() return 0 end
-
--- function script.QueryBuildInfo()
--- return center
--- end
-
--- Spring.SetUnitNanoPieces(unitID, { center })
-
---Inginition
-    --Lift rocket (rocket is slow and becomes faster)
-    --Trusters
+boolLaunching = false
+function launchAnimation()
+    boolLaunching = true
+    --Inginition
     -- plattform firebloom
+    StartThread(plattFormFireBloom)
+    --Trusters
+    showT(TablesOfPiecesGroups["Thrusters"])
+    --Lift rocket (rocket is slow and becomes faster)
+    liftRocketShowStage(300, 5000, TableOfPiecesGroups["cloud"][1], math.random(-10,10), 10)
         --Lift rocket
         -- Plattform smoke swirls
         -- Stage2 smoke Spin
@@ -129,7 +91,7 @@ function script.Deactivate() return 0 end
                 -- Glowing buckleup Crawler rooftop
                 --Moving to reassembly
                 --New Capsule
-                
+
 
 
              
