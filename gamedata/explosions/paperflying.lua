f62414683f6fc698ab01b3154ed06e0fd604bc09
PicassoCT <PicassoCT@gmx.dd>
2024-11-10 10:02:58 +0100

    paper flying in the air

---
+++ gamedata/explosions/paperflying.lua
@@ -0,0 +1,40 @@
+
+return {
+    paperwhirl = {
+            air = true,
+            class = [[CSimpleParticleSystem]],
+            count = 3,
+            ground = true,
+            water = true,
+            properties = {
+            airdrag = 0.97,
+            alwaysvisible = false,
+            colormap = [[ 
+                            0.00 0.00 0.00  0.000
+                            1.0 1.0 1.0 1.0
+                            1.0 1.0 1.0 1.0
+                            1.0 1.0 1.0 1.0
+                            0.00 0.00 0.00  0.000
+                        ]],
+                directional = true,
+                emitrot = 0,
+                emitrotspread = 10,
+                emitvector = [[0, 0.0001, 0]],
+                gravity = [[0, 0.001, 0]],
+                numparticles = 1,
+                particlelife = 320,
+                particlelifespread = 25,
+                particlesize = 30,
+                particlesizespread = 0,
+                particlespeed = 0,
+                particlespeedspread = 0,
+                pos = [[0, 10, 0]],
+                sizegrowth = 0.0,
+                sizemod = 1.00,
+                texture = [[paper]],
+                alwaysvisible = false,
+                useairlos = true
+            }
+        }
+    }
+
