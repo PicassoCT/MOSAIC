99ed0d557f95f6b43c6ecf1451c7c4fa22ae5d70
PicassoCT <PicassoCT@gmx.de>
2025-01-27 07:23:46 +0100

    Update birdsOfInfamy.lua

--- weapons/birdsOfInfamy.lua
+++ weapons/birdsOfInfamy.lua
@@ -1,5 +1,5 @@
 --- http://springrts.com/wiki/Weapon_Variables#Cannon_.28Plasma.29_Visuals
-local weaponName = "birdofinfamyglidebomb" --
+local weaponName = "icarusglidebomb" --
 local weaponDef = {
     name = "Glidebomb",
     weaponType = [[MissileLauncher]],
