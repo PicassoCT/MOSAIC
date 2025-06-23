9a42a3bfe43528b3c1e45057db2acd219f422138
PicassoCT <PicassoCT@gmx.de>
2025-06-23 14:34:42 +0200

    Revert "Auxiliary commit to revert individual files from 637eb74ba7f14cbeb768b8cc4ba84a144422e96a"
    This reverts commit 1d155c76399cbda7bb5ffb93a89f9378c7498089.

--- luarules/gadgets/game_debug.lua
+++ luarules/gadgets/game_debug.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
         date = "Sep. 2022",
         license = "GNU GPL, v2 or later",
         layer = 1,
-        enabled = true
+        enabled = false
     }
 end
 
@@ -16,10 +16,66 @@ if (gadgetHandler:IsSyncedCode()) then
     VFS.Include("scripts/lib_Animation.lua")
     VFS.Include("scripts/lib_debug.lua")
     VFS.Include("scripts/lib_mosaic.lua")
-    VFS.Include("scripts/lib_debug.lua")
 
     --Optimization
-   
+    local _recursiveCheckTable = recursiveCheckTable
+    local _getSizeInByte = getSizeInByte
+
+    function getUnitScriptEnvSize(unitID, maxDepth)
+        env = Spring.UnitScript.GetScriptEnv(unitID)
+        if env then return _getSizeInByte(env, maxDepth) end
+        return 0
+    end
+
+    local growthRatesDefIDPerUnit = {}
+    for id,def in pairs (UnitDefs) do
+        growthRatesDefIDPerUnit[id] = {}
+    end
+
+    function printUnitDefMemoryGrowth()
+        echo("")
+        echo("==================UnitDefs Average MemoryGrowth Data==================")
+        for defID, data in pairs(growthRatesDefIDPerUnit) do
+            name = UnitDefs[defID].name
+            accumulatedFactor = 0
+            for i,udata in pairs(data) do
+                accumulatedFactor = accumulatedFactor + udata.rate
+            end
+            echo(name..":"..(accumulatedFactor/count(data)))
+        end
+        echo("======================================================================")
+    end
+
+    function checkUnitScriptEnvironmentSize(frame)
+        if frame % 301 ~= 0 then return end
+        local units = Spring.GetAllUnits ( ) 
+        local spGetUnitDefID = Spring.GetUnitDefID
+
+         for i=1, #units do
+            id = units[i]
+                    defID = spGetUnitDefID(id)
+                    envSize = getUnitScriptEnvSize(id, 2)
+                    if not growthRatesDefIDPerUnit[defID][id] then growthRatesDefIDPerUnit[defID][id] = {size=envSize, rate=0} end
+                    growthRate = envSize/growthRatesDefIDPerUnit[defID][id].size
+                    growthRatesDefIDPerUnit[defID][id].rate = growthRate
+                    growthRatesDefIDPerUnit[defID][id].size = envSize
+        end
+        printUnitDefMemoryGrowth()
+    end
+
+    function checkGlobalTable(frame)
+        if frame % 100 == 0 then
+        echo("")
+        echo("==================GlobalGameTable Data==================")
+        nrElments = _recursiveCheckTable(GG, 8192, false)--frame % 1000 == 0)
+        echo("GG contains "..nrElments.." elements")
+         sizeInByte= _getSizeInByte(GG, 12)
+           echo("GG is of size ".. sizeInByte.." bytes")
+            echo("==========================================================")
+       echo("")
+       end
+      
+    end
 
     function gadget:GameFrame(frame)
         checkGlobalTable(frame)
