a6b452f8996c2f5341b4ffca866b645c39e9a530
PicassoCT <PicassoCT@gmx.de>
2025-07-04 14:14:12 +0200

    add: armybase script update

--- scripts/armybasescript.lua
+++ scripts/armybasescript.lua
@@ -2,21 +2,19 @@ include "createCorpse.lua"
 include "lib_OS.lua"
 include "lib_UnitScript.lua"
 include "lib_Animation.lua"
---include "lib_Build.lua"
 
 local TablesOfPiecesGroups = {}
-
+boolBuilding = false
 
 function script.HitByWeapon(x, z, weaponDefID, damage) end
 SIG_BLINK = 2
+SIG_BUILD = 4
 
 function script.Create()
-    --echo(UnitDefs[unitDefID].name.."has placeholder script called")
-    -- generatepiecesTableAndArrayCode(unitID)
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
-    x,y,z = Spring.GetUnitPosition(unitID)
-    --echo("{name = \"placeholder\", x = "..x..", z = "..z..", rot = 0, scale = 1.000000}")
     StartThread(blinkLights)
+    StartThread(ammoCrate)
+    Hide(buildspot)
 end
 BLINK_PASSIVE = 2000
 BLINK_ACTIVE = 1250
@@ -26,12 +24,32 @@ function blinkLights()
     Signal(SIG_BLINK)
     SetSignalMask(SIG_BLINK)
     while true do
-        showT(TablesOfPiecesGroups["lightsOn"])
-        hideT(TablesOfPiecesGroups["lightsOff"])
+        if boolBuilding == true then
+            showT(TablesOfPiecesGroups["lightsOn"])
+            hideT(TablesOfPiecesGroups["lightsOff"])
+            Sleep(blinkTime)
+            hideT(TablesOfPiecesGroups["lightsOn"])
+            showT(TablesOfPiecesGroups["lightsOff"])
+        end
         Sleep(blinkTime)
-        hideT(TablesOfPiecesGroups["lightsOn"])
-        showT(TablesOfPiecesGroups["lightsOff"])
-        Sleep(blinkTime)
+    end
+end
+
+function ammoCrate()
+    Signal(SIG_BUILD)
+    SetSignalMask(SIG_BUILD)
+    while true do
+        if boolBuilding == true then
+            WMove(Ammobox, y_axis, 100, 15)
+            WTurn(Crane, z_axis, math.rad(80), 5)
+            Hide(Ammobox)
+            Sleep(500)
+            WTurn(Crane, z_axis, math.rad(0), 5)
+            Move(Ammobox, y_axis,0, 0)
+            Sleep(5000)
+            Show(Ammobox)
+        end
+        Sleep(1000)
     end
 end
 
@@ -41,7 +59,12 @@ function script.Killed(recentDamage, _)
     return 1
 end
 
+buildspot = piece("BuildSpot")
+Crane = piece("Crane")
+Ammobox = piece("Ammobox")
+function script.QueryBuildInfo() return buildspot end
 
+Spring.SetUnitNanoPieces(unitID, {Crane})
 
 function script.StartMoving() end
 
@@ -51,8 +74,11 @@ function script.Activate() return 1 end
 
 function script.Deactivate() return 0 end
 
-function script.StartBuilding() blinkTime = BLINK_ACTIVE; SetUnitValue(COB.INBUILDSTANCE, 1) end
+function script.StartBuilding() 
+    blinkTime = BLINK_ACTIVE; 
+    boolBuilding = true; 
+    SetUnitValue(COB.INBUILDSTANCE, 1) end
 
-function script.StopBuilding() blinkTime = BLINK_PASSIVE SetUnitValue(COB.INBUILDSTANCE, 0) end
+function script.StopBuilding() blinkTime = BLINK_PASSIVE; boolBuilding = false; SetUnitValue(COB.INBUILDSTANCE, 0) end
 
 
--- units/protagon/armybase.lua
+++ units/protagon/armybase.lua
@@ -1,27 +1,27 @@
 
 local ArmyBase = Building:New{
 		corpse				= "",
-		maxDamage           = 500,
+		maxDamage           = 4000,
 		mass                = 500,
 		buildCostEnergy     = 15000,
 		buildCostMetal      = 15000,
 		explodeAs			= "none",
 
 		workerTime			= 1.0,
-		buildTime 			= 60,
+		buildTime 			= 30,
 		Builder 			= true,
 		nanocolor			= [[0.20 0.411 0.611]], --
 		CanReclaim			= false,	
 
-		YardMap 			="yyyy yyyy yyyy yyyy ",
+		YardMap 			="yyyy yyyy yyyy yyyy",
 		MetalStorage 		= 2500,
 		buildingMask 		= 8,
 		
 		maxSlope 			= 50.0,
 		levelGround 		= false,
 		blocking 			= false,
-
-		showNanoFrame		= true,
+		showNanoSpray 		= false,
+		showNanoFrame		= false,
 		script 				= "armybasescript.lua",
 		objectName        	= "armybase.dae",
 
