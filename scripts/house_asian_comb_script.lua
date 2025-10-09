41d645ecfdcb5f3621cd47adb3e3086b9ed07a00
PicassoCT <PicassoCT@gmx.dd>
2025-10-09 16:52:59 +0200

    rewrite

---
+++ scripts/house_asian_comb_script.lua
@@ -0,0 +1,45 @@
+include "createCorpse.lua"
+include "lib_OS.lua"
+include "lib_UnitScript.lua"
+include "lib_Animation.lua"
+--include "lib_Build.lua"
+
+TablesOfPiecesGroups = {}
+function script.HitByWeapon(x, z, weaponDefID, damage) end
+
+
+function script.Create()
+    echo(UnitDefs[unitDefID].name.."has placeholder script called")
+    -- generatepiecesTableAndArrayCode(unitID)
+    TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
+    -- Spring.MoveCtrl.Enable(unitID,true)
+    -- x,y,z =Spring.GetUnitPosition(unitID)
+    -- Spring.MoveCtrl.SetPosition(unitID, x,y+500,z)
+    -- StartThread(AnimationTest)
+    x,y,z = Spring.GetUnitPosition(unitID)
+    echo("{name = \"placeholder\", x = "..x..", z = "..z..", rot = 0, scale = 1.000000}")
+end
+
+
+function script.Killed(recentDamage, _)
+
+    -- createCorpseCUnitGeneric(recentDamage)
+    return 1
+end
+
+
+
+function script.StartMoving() end
+
+function script.StopMoving() end
+
+function script.Activate() return 1 end
+
+function script.Deactivate() return 0 end
+
+-- function script.QueryBuildInfo()
+-- return center
+-- end
+
+-- Spring.SetUnitNanoPieces(unitID, { center })
+
