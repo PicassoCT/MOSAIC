3f69ec3bf4bd5f4b0d94eb1a510a8cb52f98b361
PicassoCT <PicassoCT@gmx.de>
2025-10-12 12:29:04 +0200

    restored original version

--- modinfo.lua
+++ modinfo.lua
@@ -4,10 +4,9 @@ local modinfo = {
 	game = "Mosaic",
 	shortgame = "MS",
 	description = "Modular Ordanance Standard Automation & Intelligence Conflicts",
-	url = "http://springrts.com/phpbb/viewtopic.php?f=69&t=23275",
+	url = "https://github.com/PicassoCT/MOSAIC",
 	version = '$VERSION',
 	mutator = 'Official',
-	url = "https://springrts.com/phpbb/viewtopic.php?f=47&t=38699",
 	modtype = "1",
 	--Perfectionism is the death of good things
 	depend = {
