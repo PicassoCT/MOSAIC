605eab234a1e3b5713b22d45a0ddf71ca9f1d7d2
PicassoCT <PicassoCT@gmx.de>
2025-10-18 15:59:38 +0200

    VERSION{1.016}

--- scripts/lib_mosaic.lua
+++ scripts/lib_mosaic.lua
@@ -1,7 +1,7 @@
 -- ===================================================================================================================
 -- Game Configuration
  GG.unitFactor = 0.80
- GameVersion = "1.016"  
+ GameVersion = "1.016"  --UpdateFlag
  function setUnitFactor(modOptions)
     GG.unitFactor = modOptions.unitfactor or 0.8
  end
