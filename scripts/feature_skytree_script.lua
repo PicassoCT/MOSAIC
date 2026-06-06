4c89e469daa144ff39cdf33afa6a4162dd0d5caa
PicassoCT <PicassoCT@gmx.de>
2026-06-06 15:11:11 +0200

    Update feature_skytree_script.lua

--- scripts/feature_skytree_script.lua
+++ scripts/feature_skytree_script.lua
@@ -1,1 +1,45 @@
+include "createCorpse.lua"
+include "lib_OS.lua"
+include "lib_UnitScript.lua"
+include "lib_mosaic.lua"
+
+
+TablesOfPiecesGroups = {}
+function script.HitByWeapon(x, z, weaponDefID, damage) end
+
+
+function script.Create()
+    -- echo(UnitDefs[unitDefID].name.."has placeholder script called")
+    -- generatepiecesTableAndArrayCode(unitID)
+    TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
+    -- Spring.MoveCtrl.Enable(unitID,true)
+    if Spring.GetUnitDefID(unitID) == UnitDefNames["cegtest"].id then
+        x,y,z =Spring.GetUnitPosition(unitID)
+        Spring.MoveCtrl.SetPosition(unitID, x,y+500,z) 
+        echo("{name = \"placeholder\", x = "..x..", z = "..z..", rot = 0, scale = 1.000000}")
+    end
+end
+
+
+function script.Killed(recentDamage, _)
+
+    -- createCorpseCUnitGeneric(recentDamage)
+    return 1
+end
+
+
+
+function script.StartMoving() end
+
+function script.StopMoving() end
+
+function script.Activate() return 1 end
+
+function script.Deactivate() return 0 end
+
+-- function script.QueryBuildInfo()
+-- return center
+-- end
+
+-- Spring.SetUnitNanoPieces(unitID, { center })
 
