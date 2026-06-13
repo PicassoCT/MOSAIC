63fca63dac38a568f09a8d9299d86968756e93ac
PicassoCT <PicassoCT@gmx.de>
2026-06-13 15:09:40 +0200

    add: recent changes

--- scripts/feature_skytree_script.lua
+++ scripts/feature_skytree_script.lua
@@ -11,20 +11,26 @@ Tree = piece("Tree")
 function script.Create()
     TablesOfPiecesGroups = getPieceTableByNameGroups(false, true)
     x,y,z = Spring.GetUnitPosition(unitID)
-    echo("{name = \"feature_skytree\", x = "..x..", z = "..z..", rot = 0 } ")
+   -- echo("{name = \"innerCityDeco_inter5\", x = "..x..", z = "..z..", rot = 0 } ")
 
     hideAll(unitId)
+    Show(Tree)   
     showOnePiece(TablesOfPiecesGroups["Add"])
     for i=1,10 do
-        if randChanc(99) then
+        if randChance(99) then
             if randChance(90) then
-               Show(TablesOfPiecesGroups["ForrestPlate"][i])
-            else       
-                Show(TablesOfPiecesGroups["ErrorPlate"][i])
+                if TablesOfPiecesGroups["ForrestPlate"][i] then
+                    Show(TablesOfPiecesGroups["ForrestPlate"][i])
+                end
+            else 
+                if TablesOfPiecesGroups["ErrorPlate"][i] then     
+                    Show(TablesOfPiecesGroups["ErrorPlate"][i])
+                end
             end
         end
     end
-    Show(Tree)        
+     
+  
     Spring.SetUnitAlwaysVisible(unitId, true)
 end
 
